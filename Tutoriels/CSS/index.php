b12831b1a769dee30bd9806f3d388b31d0885585
Gerard KESSE <tiakagerard@hotmail.com>
2017-12-16 21:26:50 +0100

    Initial Commit

--- Tutoriels/CSS/index.php
+++ Tutoriels/CSS/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/PHP.json");
+    $m_data = GJson::Instance()->getData("data/json/CSS.json");
     
-    GConfig::Instance()->setData("title", "PHP");
+    GConfig::Instance()->setData("title", "CSS");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "PHP");
+    GConfig::Instance()->setData("view", "CSS");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
--- Tutoriels/SVN/index.php
+++ Tutoriels/SVN/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/PHP.json");
+    $m_data = GJson::Instance()->getData("data/json/SVN.json");
     
-    GConfig::Instance()->setData("title", "PHP");
+    GConfig::Instance()->setData("title", "SVN");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "PHP");
+    GConfig::Instance()->setData("view", "SVN");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
