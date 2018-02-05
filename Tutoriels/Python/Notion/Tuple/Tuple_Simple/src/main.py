21ac792aece2c94d974890358731f7c71ce3e041
Gerard KESSE <tiakagerard@hotmail.com>
2018-02-05 22:31:33 +0100

    Initial Commit

---
+++ Tutoriels/Python/Notion/Tuple/Tuple_Simple/src/main.py
@@ -0,0 +1,7 @@
+#================================================
+import GTuple
+#================================================
+print "\n### Create Data\n"
+m_data = ("Gerard", "Deborah", "Bryan", "Copernic", "Kepler")
+GTuple.showData(m_data)
+#================================================
