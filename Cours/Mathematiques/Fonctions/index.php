4533b730316615eedc3c6576f7c846786850727e
Gerard KESSE <tiakagerard@hotmail.com>
2018-02-14 11:03:13 +0100

    Initial Commit

--- Cours/Mathematiques/Fonctions/index.php
+++ Cours/Mathematiques/Fonctions/index.php
@@ -1,3 +1,30 @@
 <?php
-    require $_SERVER["DOCUMENT_ROOT"]."/php/error.php";
+    require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
+    
+    $m_data = GJson::Instance()->getData("data/json/Fonctions.json");
+    
+    GConfig::Instance()->setData("title", "Fonctions");
+    GConfig::Instance()->setData("menu", "Cours");
+    GConfig::Instance()->setData("view", "Fonctions");
+    GConfig::Instance()->setData("link", $m_data["links"]);
+    
+    require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
 ?>
+<!-- ============================================ -->
+<div class="pgBg">
+    <div class="mrga pdfBd">
+        <!-- ============================================ -->
+        <div class="pgCt10">
+            <object id="pdf" width="100%" height="640"
+            data="/lib/pdfjs/web/viewer.html?file=/Cours/Mathematiques/Fonctions/Fonctions.pdf#zoom=page-width" 
+            type="text/html">
+                La visualisation n’est pas encore disponible sur votre navigateur,
+                veillez à le mettre à jour.
+            </object>
+        </div>
+        <!-- ============================================ -->
+    </div>
+</div>
+<!-- ============================================ -->
+<?php require $_SERVER["DOCUMENT_ROOT"]."/php/footer.php"; ?>
+<!-- ============================================ -->
