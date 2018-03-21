34561519ecf0c03801771f0046ca3eddd7576dc7
Gerard KESSE <tiakagerard@hotmail.com>
2018-03-22 00:34:25 +0100

    Initial Commit

--- Tutoriels/Cpp/Apprendre_a_Programmer_en_Cpp/page/Clavier.php
+++ Tutoriels/Cpp/Apprendre_a_Programmer_en_Cpp/page/Clavier.php
@@ -69,17 +69,17 @@ src/main.cpp
 using namespace std;
 //===============================================
 int main(int argc, char** argv) {
-	string m_name;
-	int m_code;
-	
-	cout << "\n### Saisir vos identifiants:\n";
-	cout << "Nom: "; cin >> m_name;
-	cout << "Code: "; cin >> m_code;
-	
-	cout << "\n### Vous avez saisi:\n";
-	cout << "Nom: " << m_name << "\n";
-	cout << "Code: " << m_code << "\n";
-	return 0;
+    string m_name;
+    int m_code;
+    
+    cout << "\n### Saisir vos identifiants:\n";
+    cout << "Nom: "; cin >> m_name;
+    cout << "Code: "; cin >> m_code;
+    
+    cout << "\n### Vous avez saisi:\n";
+    cout << "Nom: " << m_name << "\n";
+    cout << "Code: " << m_code << "\n";
+    return 0;
 }
 //===============================================
                 </xmp></pre>
@@ -98,30 +98,31 @@ Code : la saisie du code est ignorée
             </div>
             <!-- ============================================ -->
             <!-- ============================================ -->
-            <div class="pgCr10" id="Compiler_un_projet_Cpp_avec_plusieurs_fichiers_sources">
+            <div class="pgCr10" id="Gerer_le_probleme_d_espace_lors_de_la_saisie_de_donnees">
                 <a class="bgra dibm ftfb ftsg clra pgCt10" href="#Communiquer_avec_le_clavier">
-                    Compiler un projet C++ avec plusieurs fichiers sources
+                    Gérer le problème d’espace lors de la saisie de données
                 </a>    
             </div>
             <!-- ============================================ -->
             <div class="pgCr10">
                 <div><b class="ftsg">Objectif</b></div>
                 <div class="">
-                    Compiler un projet C++ avec plusieurs fichiers sources.
+                    Gérer le problème d’espace lors de la saisie de données à partir du clavier.
                 </div>
             </div>
             <!-- ============================================ -->
             <div class="pgCr10">
                 <div><b class="ftsg">Implémentation</b></div>
                 <div class="">
-                    Afficher un message dans la console (Bonjour tout le monde).
+                    Récupérer les identifiants d’une personne saisis à partir du clavier.<br/>
+                    Récupérer le nom (m_name) et le code d’entrée (m_code) de la personne.
                 </div>
             </div>
             <!-- ============================================ -->
             <div class="pgCr10">
                 <div><b class="ftsg">Résultat</b></div>
                 <div class="txal ovfa">
-                    <img src="img/Image_01.png" alt="Image.png" />
+                    <img src="img/Image_04.png" alt="Image.png" />
                 </div>
             </div>
             <!-- ============================================ -->
@@ -129,10 +130,6 @@ Code : la saisie du code est ignorée
                 <div><b class="ftsg">Dossier projet</b></div>
 <pre class="ovfa">
 src/main.cpp
-src/manager/hello.h
-src/manager/hello.cpp
-win/bin/
-win/build/
 </pre>
             </div>
             <!-- ============================================ -->
@@ -140,176 +137,27 @@ win/build/
                 <div><b class="ftsg">src/main.cpp</b></div>
                 <pre><xmp class="ovfa prettyprint linenums">
 //===============================================
-#include "hello.h"
+#include <iostream>
+#include <string>
+//===============================================
+using namespace std;
 //===============================================
 int main(int argc, char** argv) {
-    sayHello();
+    string m_cin;
+    string m_name;
+    int m_code;
+    
+    cout << "\n### Saisir vos identifiants:\n";
+    cout << "Nom: "; getline(cin, m_cin); m_name = m_cin;
+    cout << "Code: "; getline(cin, m_cin); m_code = stoi(m_cin);
+    
+    cout << "\n### Vous avez saisi:\n";
+    cout << "Nom: " << m_name << "\n";
+    cout << "Code: " << m_code << "\n";
     return 0;
 }
 //===============================================
                 </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">src/manager/hello.h</b></div>
-                <pre><xmp class="ovfa prettyprint linenums">
-//===============================================
-#ifndef _hello_
-#define _hello_
-//===============================================
-#include <iostream>
-//===============================================
-using namespace std;
-//===============================================
-void sayHello();
-//===============================================
-#endif
-//===============================================
-                </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">src/manager/hello.cpp</b></div>
-                <pre><xmp class="ovfa prettyprint linenums">
-//===============================================
-#include "hello.h"
-//===============================================
-void sayHello() {
-    cout << "Bonjour tout le monde\n";
-}
-//===============================================
-                </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Compilation du projet</b></div>
-<pre class="ovfa">
-del /q bin\* build\*
-g++ -c ..\src\main.cpp -o build\main.o -I..\src\manager
-g++ -c ..\src\manager\hello.cpp -o build\hello.o -I..\src\manager
-g++ -o bin\GProject.exe build\main.o build\hello.o 
-</pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Exécution du projet</b></div>
-<pre class="ovfa">
-bin/GProject.exe
-</pre>
-            </div>
-            <!-- ============================================ -->
-            <!-- ============================================ -->
-            <div class="pgCr10" id="Compiler_un_projet_Cpp_a_partir_d_un_fichier_Makefile">
-                <a class="bgra dibm ftfb ftsg clra pgCt10" href="#Communiquer_avec_le_clavier">
-                    Compiler un projet C++ à partir d’un fichier Makefile
-                </a>    
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Objectif</b></div>
-                <div class="">
-                    Compiler un projet C++ à partir d’un fichier Makefile.
-                </div>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Implémentation</b></div>
-                <div class="">
-                    Afficher un message dans la console (Bonjour tout le monde).
-                </div>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Résultat</b></div>
-                <div class="txal ovfa">
-                    <img src="img/Image_01.png" alt="Image.png" />
-                </div>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Dossier projet</b></div>
-<pre class="ovfa">
-src/main.cpp
-src/manager/hello.h
-src/manager/hello.cpp
-win/Makefile
-win/bin/
-win/build/
-</pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">src/main.cpp</b></div>
-                <pre><xmp class="ovfa prettyprint linenums">
-//===============================================
-#include "hello.h"
-//===============================================
-int main(int argc, char** argv) {
-    sayHello();
-    return 0;
-}
-//===============================================
-                </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">src/manager/hello.h</b></div>
-                <pre><xmp class="ovfa prettyprint linenums">
-//===============================================
-#ifndef _hello_
-#define _hello_
-//===============================================
-#include <iostream>
-//===============================================
-using namespace std;
-//===============================================
-void sayHello();
-//===============================================
-#endif
-//===============================================
-                </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">src/manager/hello.cpp</b></div>
-                <pre><xmp class="ovfa prettyprint linenums">
-//===============================================
-#include "hello.h"
-//===============================================
-void sayHello() {
-    cout << "Bonjour tout le monde\n";
-}
-//===============================================
-                </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">win/Makefile</b></div>
-                <pre><xmp class="ovfa prettyprint linenums">
-all: build\main.o build\hello.o
-    g++ -o bin\GProject.exe build\main.o build\hello.o
-build\main.o: ..\src\main.cpp
-    g++ -c ..\src\main.cpp -o build\main.o -I..\src\manager
-build\hello.o: ..\src\manager\hello.cpp
-    g++ -c ..\src\manager\hello.cpp -o build\hello.o -I..\src\manager
-clean:
-    del /q bin\* build\*
-                </xmp></pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Compilation du projet</b></div>
-<pre class="ovfa">
-mingw32-make clean
-mingw32-make
-</pre>
-            </div>
-            <!-- ============================================ -->
-            <div class="pgCr10">
-                <div><b class="ftsg">Exécution du projet</b></div>
-<pre class="ovfa">
-bin/GProject.exe
-</pre>
             </div>
             <!-- ============================================ -->
             <!-- ============================================ -->
