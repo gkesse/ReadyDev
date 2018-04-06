811215acd518fa32170ebf559f5340e48fe14e71
Gerard KESSE <tiakagerard@hotmail.com>
2018-04-06 15:02:19 +0200

    Initial Commit

--- Tutoriels/Cpp/Cpp/code/Boucle_While/src/main.cpp
+++ Tutoriels/Cpp/Cpp/code/Boucle_While/src/main.cpp
@@ -10,12 +10,16 @@ int main(int argc, char** argv) {
     
     cout << "### Table de multiplication par 9\n\n";
     
-	for(int i = 0; i <= ROW_MAX; i++) {
+    int i = 0;
+    
+	while(i <= ROW_MAX) {
         int m_data = i * FACTOR;
         cout << setw(2) << i << " x ";
         cout << FACTOR << " = ";
         cout << setw(2) << m_data << "\n";
+        i++;
     }
+    
 	return 0;
 }
 //===============================================
