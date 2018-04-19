49a102b3a8cb96ae12b9b2b67ff6f4c327f5cbca
Gerard KESSE <tiakagerard@hotmail.com>
2018-04-19 15:51:13 +0200

    Initial Commit

--- Tutoriels/Cpp/OpenGL/code/src/main.cpp
+++ Tutoriels/Cpp/OpenGL/code/src/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char** argv) {
     GConfig::Instance()->setData("BACKGROUND_FLAG", "TRUE");
     GConfig::Instance()->setData("BACKGROUND_COLOR", "5;16;57;255");
     GConfig::Instance()->setData("DRAW_FLAG", "TRUE");
-    GConfig::Instance()->setData("DRAW_TYPE", "BIND");
+    GConfig::Instance()->setData("DRAW_TYPE", "LAYOUT");
+    GConfig::Instance()->setData("SHADER_LOAD_METHOD", "LAYOUT");
     GConfig::Instance()->setData("COLOR_VAL", "55;63;96;255");
-    GConfig::Instance()->setData("SHADER_LOAD_METHOD", "BIND");
     GWindow::Instance()->show(&argc, argv);
     return 0;
 }
