7c31833636e5b2dfd06aab4eab3a9dc236cdd25c
Gerard KESSE <tiakagerard@hotmail.com>
2018-04-25 13:14:53 +0200

    Initial Commit

--- Tutoriels/Cpp/OpenGL/code/src/manager/GDrawSDLCube.cpp
+++ Tutoriels/Cpp/OpenGL/code/src/manager/GDrawSDLCube.cpp
@@ -38,28 +38,28 @@ void GDrawSDLCube::initCamera(int width, int height) {
 }
 //===============================================
 void GDrawSDLCube::draw() {
-    float lVertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
+    float lVertices[] = {
+        -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f
+    };
     float lColors[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f
     };
     m_modelView = glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     glUseProgram(m_program);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, lVertices);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, lVertices);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, lColors);
     glEnableVertexAttribArray(1);
-    m_modelView = glm::scale(m_modelView, glm::vec3(0.8f,0.8f,0.8f));
-    m_modelView = glm::translate(m_modelView, glm::vec3(0.0f,0.5f,0.0f));
+    //m_modelView = glm::scale(m_modelView, glm::vec3(0.8f,0.8f,0.8f));
+    //m_modelView = glm::translate(m_modelView, glm::vec3(0.0f,0.5f,0.0f));
+    //m_modelView = glm::rotate(m_modelView, glm::radians(180.0f), glm::vec3(0.0f,0.0f,1.0f));
     GShader::Instance()->setUniform("ModelViewMatrix", m_modelView);
     GShader::Instance()->setUniform("ProjectionMatrix", m_projection);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    m_modelView = glm::rotate(m_modelView, glm::radians(180.0f), glm::vec3(0.0f,0.0f,1.0f));
-    m_modelView = glm::translate(m_modelView, glm::vec3(0.0f,1.0f,0.0f));
-    GShader::Instance()->setUniform("ModelViewMatrix", m_modelView);
-    GShader::Instance()->setUniform("ProjectionMatrix", m_projection);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(0);
     glUseProgram(0);
