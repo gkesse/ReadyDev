6ae42473f0e12458e79d3683f864c50e0b6b2d43
Gerard KESSE <tiakagerard@hotmail.com>
2018-06-20 17:01:25 +0200

    Initial Commit

--- Tutoriels/Cpp/Co/Console/Variable_Formatage/src/main.c
+++ Tutoriels/Cpp/Co/Console/Variable_Formatage/src/main.c
@@ -8,13 +8,13 @@ int main(int argc, char** argv) {
     printf("%-38s: %u\n", "Entier Naturel", 987);
     printf("%-38s: %o\n", "Entier Octal ", 987);
     printf("%-38s: %x\n", "Entier Hexadecimal MIN", 987);
-    printf("%-38s: %X\n", "Entier Hexadecimal MAJ" 987);
+    printf("%-38s: %X\n", "Entier Hexadecimal MAJ", 987);
     printf("%-38s: %f\n", "Reel", 987.654);
     printf("%-38s: %lf\n", "Double", 987.654);
     printf("%-38s: %e\n", "Notation Scientifique MIN", 987.654);
     printf("%-38s: %E\n", "Notation Scientifique MAJ", 987.654);
-    printf("%-38s: %g\n", "Shorter Float (987.654)", 987.654);
-    printf("%-38s: %g\n", "Shorter Scientific (9.876540E+002)", 9.876540E+002);
+    printf("%-38s: %g\n", "Reel Court", 987.654);
+    printf("%-38s: %a\n", "Reel Hexadecimal", 987.654);
     
     return 0;
 }
