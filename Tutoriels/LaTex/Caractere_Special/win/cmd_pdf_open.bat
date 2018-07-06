c998e5ce58f91e62fc1c66b092534adffbf231c1
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-06 20:17:30 +0200

    Initial Commit

---
+++ Tutoriels/LaTex/Caractere_Special/win/cmd_pdf_open.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set PATH=C:\Program Files (x86)\Adobe\Reader 11.0\Reader
+
+set GPDF=C:\Users\sabine\Downloads\Gerard\Programs\ReadyDev\Tutoriels\LaTex\Caractere_Special\win\pdf
+
+AcroRd32 %GPDF%\main.pdf
