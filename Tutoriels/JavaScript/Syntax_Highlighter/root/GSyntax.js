1513f79fb10167c4fa53c508538fca01f72ce940
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-08 13:51:13 +0200

    Initial Commit

--- Tutoriels/JavaScript/Syntax_Highlighter/root/GSyntax.js
+++ Tutoriels/JavaScript/Syntax_Highlighter/root/GSyntax.js
@@ -15,28 +15,49 @@ var GSyntax = (function() {
                 if(!lPrintSyntaxMap.length) return;
                 for(var i = 0; i < lPrintSyntaxMap.length; i++) {
                     var lData = lPrintSyntaxMap[i].innerHTML;
-                    this.syntaxHtml(lData);
+                    var lSyntax = this.syntaxHtml(lData);
+                    lPrintSyntaxMap[i].innerHTML = lSyntax;
                 }
             },
             //===============================================
             syntaxHtml: function(data) {
-                this.extract(data, "&lt;!--", "--&gt;", this.commentMode);
+                var lExtract = new this.extract(data, "&lt;!--", "--&gt;", this.commentMode, "W3HTMLCOMMENTPOS");
+                var lData = lExtract.data;
+                var lDataMap = lExtract.map;
+                for(var i = 0; i < lDataMap.length; i++) {
+                    lData = lData.replace("W3HTMLCOMMENTPOS", lDataMap[i]);
+                }
+                var lSyntax = this.syntaxCode(lData);
+                return lSyntax;
             },
             //===============================================
-            extract: function(data, start, end, func) {
+            syntaxCode: function(data) {
+                var lData = "";
+                lData += "<div style='display:inline-block;font-family:courier'>";
+                lData += data;
+                lData += "</div>";
+                return lData;
+            },
+            //===============================================
+            extract: function(data, start, end, func, replace) {
                 var lDataMap = [];
-                if(data.search(start) > -1) {
+                while(data.search(start) > -1) {
                     var lSearch = data.search(start);
                     var lIndexOf = data.indexOf(end, lSearch);
                     var lSubstring = data.substring(lSearch, lIndexOf + (end.length));
                     var lFunc = func(lSubstring);
-                    alert(lFunc);
+                    lDataMap.push(lFunc);
+                    var lSubstring2 = data.substring(0, lSearch);
+                    var lSubstring3 = data.substring(lIndexOf + (end.length));
+                    data = lSubstring2 + replace + lSubstring3;
                 }
+                this.data = data;
+                this.map = lDataMap;
             },
             //===============================================
             commentMode: function(data) {
                 var lData = "";
-                lData += "<span style='color:lime;'>";
+                lData += "<span style='color:limegreen;'>";
                 lData += data;
                 lData += "</span>";
                 return lData;
--- Tutoriels/JavaScript/Syntax_Highlighter/root/index.php
+++ Tutoriels/JavaScript/Syntax_Highlighter/root/index.php
@@ -14,6 +14,7 @@
             &lt;html&gt;<br>
             &lt;body&gt;<br>
             <br>
+            &lt;!-- Voici mon deuxième commentaire --&gt;<br>
             &lt;h1&gt;Testing an HTML Syntax Highlighter&lt;/h2&gt;<br>
             &lt;p&gt;Hello world!&lt;/p&gt;<br>
             &lt;a href="https://www.w3schools.com"&gt;Back to School&lt;/a&gt;<br>
