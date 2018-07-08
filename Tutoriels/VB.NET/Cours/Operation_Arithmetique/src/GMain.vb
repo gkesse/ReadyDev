58d6f0aef9f6cba4aae4a0b35faba931d1269f61
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-08 03:06:44 +0200

    Initial Commit

--- Tutoriels/VB.NET/Cours/Operation_Arithmetique/src/GMain.vb
+++ Tutoriels/VB.NET/Cours/Operation_Arithmetique/src/GMain.vb
@@ -7,13 +7,13 @@ Module GModule
         Dim A As Integer = 22
         Dim B As Integer = 7
         
-        Console.Write(String.Format("{0,-10} : {1}", "Addition", A + B) + CR)
-        Console.Write(String.Format("{0,-10} : {1}", "Soustraction", A - B) + CR)
-        Console.Write(String.Format("{0,-10} : {1}", "Multiplication", A * B) + CR)
-        Console.Write(String.Format("{0,-10} : {1}", "Division", A / B) + CR)
-        Console.Write(String.Format("{0,-10} : {1}", "Division entière", A \ B) + CR)
-        Console.Write(String.Format("{0,-10} : {1}", "Puissance", A ^ 2) + CR)
-        Console.Write(String.Format("{0,-10} : {1}", "Modulo", A mod B) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Addition", A + B) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Soustraction", A - B) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Multiplication", A * B) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Division", A / B) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Division entière", A \ B) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Puissance", A ^ 2) + CR)
+        Console.Write(String.Format("{0,-20} : {1}", "Modulo", A mod B) + CR)
     End Sub
 End Module
 '================================================
