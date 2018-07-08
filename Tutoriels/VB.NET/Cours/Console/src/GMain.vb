3d81ab92a76071afcd690f218583c9f03e8044d3
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-08 02:44:15 +0200

    Initial Commit

---
+++ Tutoriels/VB.NET/Cours/Console/src/GMain.vb
@@ -0,0 +1,9 @@
+'================================================
+Module GModule
+    Sub Main()
+        Dim CR As String = Environment.newLine
+        Console.Write("### Console" + CR + CR)
+        Console.Write("Bonjour tout le monde" + CR)
+    End Sub
+End Module
+'================================================
---
+++ Tutoriels/VB.NET/Cours/Operation_Arithmetique/src/GMain.vb
@@ -0,0 +1,19 @@
+'================================================
+Module GModule
+    Sub Main()
+        Dim CR As String = Environment.newLine
+        Console.Write("### Variable" + CR + CR)
+        
+        Dim A As Integer = 22
+        Dim B As Integer = 7
+        
+        Console.Write(String.Format("{0,-10} : {1}", "Addition", A + B) + CR)
+        Console.Write(String.Format("{0,-10} : {1}", "Soustraction", A - B) + CR)
+        Console.Write(String.Format("{0,-10} : {1}", "Multiplication", A * B) + CR)
+        Console.Write(String.Format("{0,-10} : {1}", "Division", A / B) + CR)
+        Console.Write(String.Format("{0,-10} : {1}", "Division entière", A \ B) + CR)
+        Console.Write(String.Format("{0,-10} : {1}", "Puissance", A ^ 2) + CR)
+        Console.Write(String.Format("{0,-10} : {1}", "Modulo", A mod B) + CR)
+    End Sub
+End Module
+'================================================
