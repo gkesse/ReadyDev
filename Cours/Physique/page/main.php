117399a0b92e9dffd53176ec0e08b05c8d8a33c0
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-18 01:37:37 +0200

    Initial Commit

---
+++ Cours/Physique/page/main.php
@@ -0,0 +1,29 @@
+<div class="Parallax GParallax1"><div class="Img Binary"><div class="Caption"><a href="#"><div class="Text">Physique</div></a></div></div><div class="Body2 Orange">La <b>Physique</b> est la science de modélisation des phénomènes de l'univers.<br>Le but de ce cours est de vous apprendre la <b>Physique</b> appliquées aux sciences de l'ingénieur.<br>Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary1"><div class="Body0" id="Loader_1530820262574"><div class="Row26">Summary 1</div></div><script>loadSummary1("Loader_1530820262574");</script></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Effet Doppler"><a class="Link3" href="#">Effet Doppler</a></h1><div class="Body3">L'<b>effet Doppler</b> est le <span class="GColor1" style="color:lime;">décalage de fréquence</span> observé entre un émetteur et un récepteur lorsque la distance les séparant varie au cours du temps. <br>Le but de cette section est de vous apprendre à modéliser l'<b>effet Doppler</b>.<br>Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1531870581126"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1531870581126");</script></div><br><div class="Img3"><img src="img/Doppler_Effect.gif" alt="img/Doppler_Effect.gif"></div><br><h2 class="Title7 GTitle2" id="Effet Doppler-Lorsque l'émetteur se rapproche du récepteur"><a class="Link9" href="#Effet Doppler">Lorsque l'émetteur se rapproche du récepteur</a></h2><br><h3 class="Title8 GTitle3">Schéma explicatif</h3><div class="Img3"><img src="img/Effet_Doppler.png" alt="img/Effet_Doppler.png"></div><br><div class="Formula GFormula1"><div class="Formula2">$c\rightarrow$ vitesse d'onde émise<br>
+$T_e\rightarrow$ période d'onde émise<br>
+$T_r\rightarrow$ période d'onde apparente reçue par le récepteur<br>
+$v_E\rightarrow$ vitesse de l'émetteur<br>
+$d_0\rightarrow$ distance parcourue par l'onde émise pendant $T_e$<br>
+$d_1\rightarrow$ distance parcourue par l'émetteur pendant $T_e$<br>
+$d_2\rightarrow$ distance apparente parcourue par l'onde émise pendant $T_r$<br>
+</div></div><br><h3 class="Title8 GTitle3">Fréquence reçu par le récepteur</h3><div class="Formula GFormula1"><div class="Formula2">\begin{eqnarray} 
+d_0 &amp;=&amp; cT_e\\\\ 
+d_1 &amp;=&amp; v_E T_e\\\\ 
+d_2 &amp;=&amp; d_0-d_1\\\\ 
+d_2 &amp;=&amp; cT_e-v_E T_e\\\\ 
+d_2 &amp;=&amp; (c-v_E ) T_e\\\\ 
+d_2 &amp;=&amp; cT_r\\\\ 
+cT_r &amp;=&amp; (c-v_E ) T_e\\\\ 
+T_r &amp;=&amp; T_e  \frac{(c-v_E)}{c}
+\end{eqnarray}
+</div></div><div class="Formula GFormula1"><div class="Formula2">\begin{eqnarray} 
+f_r &amp;=&amp; f_e  \frac{c}{(c-v_E )}
+\end{eqnarray}
+</div></div><br><h3 class="Title8 GTitle3">Comparaison</h3><div class="Formula GFormula1"><div class="Formula2">\begin{eqnarray}
+c &amp;&gt;&amp; c-v_E\\\\
+\frac{c}{(c-v_E )} &amp;&gt;&amp; 1\\\\
+f_e  \frac{c}{(c-v_E )} &amp;&gt;&amp; f_e
+\end{eqnarray}
+</div></div><div class="Formula GFormula1"><div class="Formula2">\begin{eqnarray}
+f_r &amp;&gt;&amp; f_e
+\end{eqnarray}
+</div></div><br><h2 class="Title7 GTitle2" id="Effet Doppler-Lorsque l'émetteur s’éloigne du récepteur"><a class="Link9" href="#Effet Doppler">Lorsque l'émetteur s’éloigne du récepteur</a></h2><br><div class="Img3"><img src="img/Effet_Doppler_02.png" alt="img/Effet_Doppler_02.png"></div><br><h2 class="Title7 GTitle2" id="Effet Doppler-Lorsque le récepteur se rapproche de l'émetteur"><a class="Link9" href="#Effet Doppler">Lorsque le récepteur se rapproche de l'émetteur</a></h2><br><div class="Img3"><img src="img/Effet_Doppler_03.png" alt="img/Effet_Doppler_03.png"></div><br><h2 class="Title7 GTitle2" id="Effet Doppler-Lorsque le récepteur s’éloigne de l'émetteur"><a class="Link9" href="#Effet Doppler">Lorsque le récepteur s’éloigne de l'émetteur</a></h2><br><div class="Img3"><img src="img/Effet_Doppler_04.png" alt="img/Effet_Doppler_04.png"></div><br><h2 class="Title7 GTitle2" id="Effet Doppler-Lorsque l'émetteur et le récepteur se rapprochent"><a class="Link9" href="#Effet Doppler">Lorsque l'émetteur et le récepteur se rapprochent</a></h2><br><div class="Img3"><img src="img/Effet_Doppler_05.png" alt="img/Effet_Doppler_05.png"></div></div></div></div></div><br>
