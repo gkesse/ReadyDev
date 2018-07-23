cd3ff0506c9157d7bd29438b41fbae33680e3513
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-23 20:21:44 +0200

    Initial Commit

--- Tutoriels/Cpp/Co/Pattern/Classe/src/main.c
+++ Tutoriels/Cpp/Co/Pattern/Classe/src/main.c
@@ -7,6 +7,9 @@ int main(int argc, char** argv) {
     GClass* lObj2 = GClass_Constructor2("Bonjour tout le monde");
     GClass_Print_Data(lObj);
     GClass_Print_Data(lObj2);
+    GClass_Set_Data(lObj, "Bonjour Gerard");
+    GClass_Print_Data(lObj);
+    printf("Lecture: %s\n", GClass_Get_Data(lObj2));
     GClass_Destructor(lObj);
     GClass_Destructor(lObj2);
     return 0;
--- Tutoriels/Cpp/Co/Pattern/manager/GClass2.h
+++ Tutoriels/Cpp/Co/Pattern/manager/GClass2.h
@@ -5,18 +5,25 @@
 #include "GInclude.h"
 //===============================================
 typedef struct {
+    //===============================================
+    void (*Print_Data)(GClass* obj);
+    void (*Set_Data)(GClass* obj, const char* data);
+    void (*Get_Data)(GClass* obj);
+    //===============================================
     char m_data[50];
 } GClass;
 //===============================================
 GClass* GClass_Constructor() {
     GClass* lObj = (GClass*)malloc(sizeof(GClass));
     sprintf(lObj->m_data, "%s", "_NONE_");
+    GClass_Init(lObj);
     return lObj; 
 }
 //===============================================
 GClass* GClass_Constructor2(const char* data) {
     GClass* lObj = (GClass*)malloc(sizeof(GClass));
     sprintf(lObj->m_data, "%s", data);
+    GClass_Init(lObj);
     return lObj; 
 }
 //===============================================
@@ -37,6 +44,12 @@ void GClass_Set_Data(GClass* obj, const char* data) {
 //===============================================
 char* GClass_Get_Data(GClass* obj) {
     return obj->m_data;
+}
+//===============================================
+char* GClass_Init(GClass* obj) {
+    obj->Print_Data = GClass_Print_Data;
+    obj->Set_Data = GClass_Set_Data;
+    obj->Get_Data = GClass_Get_Data;
 }
 //===============================================
 #endif
