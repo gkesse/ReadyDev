24e6df1be87b240c3a13314ea4ca8b7df6c564bc
Gerard KESSE <tiakagerard@hotmail.com>
2018-07-27 22:25:01 +0200

    Initial Commit

---
+++ Tutoriels/Cpp/Co/Pattern/Chaine/src/main.c
@@ -0,0 +1,15 @@
+//===============================================
+#include "GString.h"
+//===============================================
+int main(int argc, char** argv) {
+    printf("\n### Chaine Taille\n\n");
+    printf("Taille: Bonjour: %d\n", GString_Instance().Size("Bonjour"));
+
+    printf("\n### Est Egal\n\n");
+    printf("Est Egal: Bonjour:Bonjour: %d\n", GString_Instance().Is_Equal("Bonjour", "Bonjour"));
+    printf("Est Egal: Bonjour:bonjour: %d\n", GString_Instance().Is_Equal("Bonjour", "bonjour"));
+    printf("Est Egal: Bonjour:Bon: %d\n", GString_Instance().Is_Equal("Bonjour", "bonjour"));
+    
+    return 0;
+}
+//===============================================
