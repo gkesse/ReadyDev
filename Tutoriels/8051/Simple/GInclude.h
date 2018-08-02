0bdbaf664c5da02b3e2702cc487eeed04785543a
Gerard KESSE <tiakagerard@hotmail.com>
2018-08-03 01:50:02 +0200

    Initial Commit

--- Tutoriels/8051/Simple/GInclude.h
+++ Tutoriels/8051/Simple/GInclude.h
@@ -11,5 +11,12 @@ typedef unsigned long ulong;
 #define TRUE (1)
 #define FALSE (0)
 //===============================================
+#define OSC_FREQ (11059200)
+#define OSC_PER_INST (12)
+//===============================================
+#define INTERRUPT_TIMER_T0 1
+#define INTERRUPT_TIMER_T1 3
+#define INTERRUPT_TIMER_T2 5
+//===============================================
 #endif
 //===============================================
