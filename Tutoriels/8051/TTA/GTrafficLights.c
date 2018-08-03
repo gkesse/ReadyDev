f0a8afdf0fb18f4f560968c7989ca5acc794d24a
Gerard KESSE <tiakagerard@hotmail.com>
2018-08-04 01:37:35 +0200

    Initial Commit

--- Tutoriels/8051/TTA/GTrafficLights.c
+++ Tutoriels/8051/TTA/GTrafficLights.c
@@ -2,33 +2,35 @@
 #include "GTrafficLights.h"
 #include "GPortDef.h"
 //===============================================
-typedef enum eGLight GLight;
-//===============================================
 enum eGLight {RED, RED_AMBER, RED_RED_1, GREEN, AMBER, RED_RED_2};
 //===============================================
-static GLight gLight_State_L1;
-static GLight gLight_State_L2;
+typedef enum eGLight eGLight;
 //===============================================
-static uint gState_Light_L2_Time;
-static uint gState_Light_L1_Time;
+static eGLight gLight_L1_State = RED;
+static eGLight gLight_L2_State = GREEN;
 //===============================================
-void GState_Init() {
-    gLight_State_L1 = RED;
-    gLight_State_L2 = GREEN;
+static uint gLight_L1_Time = 0;
+static uint gLight_L2_Time = 0;
+//===============================================
+void GTrafficLights_Init() {
+    LIGHT_RED_L1 = LIGHT_OFF;
+    LIGHT_AMBER_L1 = LIGHT_OFF;
+    LIGHT_GREEN_L1 = LIGHT_OFF;
     
-    gState_Light_L1_Time = 0;
-    gState_Light_L2_Time = 0;
+    LIGHT_RED_L2 = LIGHT_OFF;
+    LIGHT_AMBER_L2 = LIGHT_OFF;
+    LIGHT_GREEN_L2 = LIGHT_OFF;
 }
 //===============================================
-void GState_Light_L1() {
-    switch(gLight_State_L1) {
+void GTrafficLights_Light_L1() {
+    switch(gLight_L1_State) {
         case RED: {
             LIGHT_RED_L1 = LIGHT_ON;
             LIGHT_AMBER_L1 = LIGHT_OFF;
             LIGHT_GREEN_L1 = LIGHT_OFF;
-            if(++gState_Light_L1_Time >= LIGHT_RED_DURATION) {
-                gLight_State_L1 = RED_AMBER;
-                gState_Light_L1_Time = 0;    
+            if(++gLight_L1_Time >= LIGHT_RED_DURATION) {
+                gLight_L1_State = RED_AMBER;
+                gLight_L1_Time = 0;    
             } 
             break;
         }
@@ -36,9 +38,9 @@ void GState_Light_L1() {
             LIGHT_RED_L1 = LIGHT_ON;
             LIGHT_AMBER_L1 = LIGHT_OFF;
             LIGHT_GREEN_L1 = LIGHT_OFF;
-            if(++gState_Light_L1_Time >= LIGHT_RED_AMBER_DURATION) {
-                gLight_State_L1 = RED_RED_1;
-                gState_Light_L1_Time = 0;    
+            if(++gLight_L1_Time >= LIGHT_RED_AMBER_DURATION) {
+                gLight_L1_State = RED_RED_1;
+                gLight_L1_Time = 0;    
             } 
             break;
         }
@@ -46,9 +48,9 @@ void GState_Light_L1() {
             LIGHT_RED_L1 = LIGHT_ON;
             LIGHT_AMBER_L1 = LIGHT_OFF;
             LIGHT_GREEN_L1 = LIGHT_OFF;
-            if(++gState_Light_L1_Time >= LIGHT_RED_RED_1_DURATION) {
-                gLight_State_L1 = GREEN;
-                gState_Light_L1_Time = 0;    
+            if(++gLight_L1_Time >= LIGHT_RED_RED_1_DURATION) {
+                gLight_L1_State = GREEN;
+                gLight_L1_Time = 0;    
             } 
             break;
         }
@@ -56,9 +58,9 @@ void GState_Light_L1() {
             LIGHT_RED_L1 = LIGHT_OFF;
             LIGHT_AMBER_L1 = LIGHT_OFF;
             LIGHT_GREEN_L1 = LIGHT_ON;
-            if(++gState_Light_L1_Time >= LIGHT_GREEN_DURATION) {
-                gLight_State_L1 = AMBER;
-                gState_Light_L1_Time = 0;    
+            if(++gLight_L1_Time >= LIGHT_GREEN_DURATION) {
+                gLight_L1_State = AMBER;
+                gLight_L1_Time = 0;    
             } 
             break;
         }
@@ -66,9 +68,9 @@ void GState_Light_L1() {
             LIGHT_RED_L1 = LIGHT_OFF;
             LIGHT_AMBER_L1 = LIGHT_ON;
             LIGHT_GREEN_L1 = LIGHT_OFF;
-            if(++gState_Light_L1_Time >= LIGHT_AMBER_DURATION) {
-                gLight_State_L1 = RED_RED_2;
-                gState_Light_L1_Time = 0;    
+            if(++gLight_L1_Time >= LIGHT_AMBER_DURATION) {
+                gLight_L1_State = RED_RED_2;
+                gLight_L1_Time = 0;    
             } 
             break;
         }
@@ -76,9 +78,9 @@ void GState_Light_L1() {
             LIGHT_RED_L1 = LIGHT_ON;
             LIGHT_AMBER_L1 = LIGHT_OFF;
             LIGHT_GREEN_L1 = LIGHT_OFF;
-            if(++gState_Light_L1_Time >= LIGHT_RED_RED_2_DURATION) {
-                gLight_State_L1 = RED;
-                gState_Light_L1_Time = 0;    
+            if(++gLight_L1_Time >= LIGHT_RED_RED_2_DURATION) {
+                gLight_L1_State = RED;
+                gLight_L1_Time = 0;    
             } 
             break;
         }
@@ -86,15 +88,15 @@ void GState_Light_L1() {
 
 }
 //===============================================
-void GState_Light_L2() {
-    switch(gLight_State_L2) {
+void GTrafficLights_Light_L2() {
+    switch(gLight_L2_State) {
         case RED: {
             LIGHT_RED_L2 = LIGHT_ON;
             LIGHT_AMBER_L2 = LIGHT_OFF;
             LIGHT_GREEN_L2 = LIGHT_OFF;
-            if(++gState_Light_L2_Time >= LIGHT_RED_DURATION) {
-                gLight_State_L2 = RED_AMBER;
-                gState_Light_L2_Time = 0;    
+            if(++gLight_L2_Time >= LIGHT_RED_DURATION) {
+                gLight_L2_State = RED_AMBER;
+                gLight_L2_Time = 0;    
             } 
             break;
         }
@@ -102,9 +104,9 @@ void GState_Light_L2() {
             LIGHT_RED_L2 = LIGHT_ON;
             LIGHT_AMBER_L2 = LIGHT_OFF;
             LIGHT_GREEN_L2 = LIGHT_OFF;
-            if(++gState_Light_L2_Time >= LIGHT_RED_AMBER_DURATION) {
-                gLight_State_L2 = RED_RED_1;
-                gState_Light_L2_Time = 0;    
+            if(++gLight_L2_Time >= LIGHT_RED_AMBER_DURATION) {
+                gLight_L2_State = RED_RED_1;
+                gLight_L2_Time = 0;    
             } 
             break;
         }
@@ -112,9 +114,9 @@ void GState_Light_L2() {
             LIGHT_RED_L2 = LIGHT_ON;
             LIGHT_AMBER_L2 = LIGHT_OFF;
             LIGHT_GREEN_L2 = LIGHT_OFF;
-            if(++gState_Light_L2_Time >= LIGHT_RED_RED_1_DURATION) {
-                gLight_State_L2 = GREEN;
-                gState_Light_L2_Time = 0;    
+            if(++gLight_L2_Time >= LIGHT_RED_RED_1_DURATION) {
+                gLight_L2_State = GREEN;
+                gLight_L2_Time = 0;    
             } 
             break;
         }
@@ -122,9 +124,9 @@ void GState_Light_L2() {
             LIGHT_RED_L2 = LIGHT_OFF;
             LIGHT_AMBER_L2 = LIGHT_OFF;
             LIGHT_GREEN_L2 = LIGHT_ON;
-            if(++gState_Light_L2_Time >= LIGHT_GREEN_DURATION) {
-                gLight_State_L2 = AMBER;
-                gState_Light_L2_Time = 0;    
+            if(++gLight_L2_Time >= LIGHT_GREEN_DURATION) {
+                gLight_L2_State = AMBER;
+                gLight_L2_Time = 0;    
             } 
             break;
         }
@@ -132,9 +134,9 @@ void GState_Light_L2() {
             LIGHT_RED_L2 = LIGHT_OFF;
             LIGHT_AMBER_L2 = LIGHT_ON;
             LIGHT_GREEN_L2 = LIGHT_OFF;
-            if(++gState_Light_L2_Time >= LIGHT_AMBER_DURATION) {
-                gLight_State_L2 = RED_RED_2;
-                gState_Light_L2_Time = 0;    
+            if(++gLight_L2_Time >= LIGHT_AMBER_DURATION) {
+                gLight_L2_State = RED_RED_2;
+                gLight_L2_Time = 0;    
             } 
             break;
         }
@@ -142,9 +144,9 @@ void GState_Light_L2() {
             LIGHT_RED_L2 = LIGHT_ON;
             LIGHT_AMBER_L2 = LIGHT_OFF;
             LIGHT_GREEN_L2 = LIGHT_OFF;
-            if(++gState_Light_L2_Time >= LIGHT_RED_RED_2_DURATION) {
-                gLight_State_L2 = RED;
-                gState_Light_L2_Time = 0;    
+            if(++gLight_L2_Time >= LIGHT_RED_RED_2_DURATION) {
+                gLight_L2_State = RED;
+                gLight_L2_Time = 0;    
             } 
             break;
         }
--- Tutoriels/8051/TTA/GTrafficLights.h
+++ Tutoriels/8051/TTA/GTrafficLights.h
@@ -5,8 +5,8 @@
 #include "GInclude.h"
 //===============================================
 void GTrafficLights_Init();
-void GTrafficLights_Light_L1_Update();
-void GTrafficLights_Light_L2_Update();
+void GTrafficLights_Light_L1();
+void GTrafficLights_Light_L2();
 //===============================================
 #endif
 //===============================================
