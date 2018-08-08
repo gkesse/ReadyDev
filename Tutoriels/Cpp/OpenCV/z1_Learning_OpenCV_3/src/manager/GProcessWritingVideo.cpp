772b59bf834581ca8a382d343ba3e477058b5c29
Gerard KESSE <tiakagerard@hotmail.com>
2018-08-08 11:51:24 +0200

    Initial Commit

---
+++ Tutoriels/Cpp/OpenCV/z1_Learning_OpenCV_3/src/manager/GProcessWritingVideo.cpp
@@ -0,0 +1,35 @@
+//===============================================
+#include "GProcessCameraUsb.h"
+//===============================================
+GProcessCameraUsb* GProcessCameraUsb::m_instance = 0;
+//===============================================
+GProcessCameraUsb::GProcessCameraUsb() {
+
+}
+//===============================================
+GProcessCameraUsb::~GProcessCameraUsb() {
+
+}
+//===============================================
+GProcessCameraUsb* GProcessCameraUsb::Instance() {
+    if(m_instance == 0) {
+        m_instance = new GProcessCameraUsb;
+    }
+    return m_instance;
+}
+//===============================================
+void GProcessCameraUsb::run() {
+    cv::namedWindow("Camera USB | ReadyDev", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture lCap;
+    lCap.open(0);
+    if(lCap.isOpened() == false) return;
+    cv::Mat lFrame;
+
+    while(1) {
+        lCap >> lFrame;
+        if(lFrame.empty()) break;
+        cv::imshow("Camera USB | ReadyDev", lFrame);
+        if(cv::waitKey(33) >= 0) break;
+    }
+}
+//===============================================
