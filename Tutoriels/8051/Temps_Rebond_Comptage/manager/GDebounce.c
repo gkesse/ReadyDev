f27c6ca7e2d1a8b60be0f24d36557270e2f10d62
Gerard KESSE <tiakagerard@hotmail.com>
2018-08-12 23:00:25 +0200

    Initial Commit

--- Tutoriels/8051/Temps_Rebond_Comptage/manager/GDebounce.c
+++ Tutoriels/8051/Temps_Rebond_Comptage/manager/GDebounce.c
@@ -4,37 +4,35 @@
 #include "GDelay.h"
 //===============================================
 static bit gDebounce_State = 0;
+static uint gDebounce_Count = 0;
 //===============================================
 bit GDebounce_Read_Input(uchar debounce);
-void GDebounce_Write_Output(bit state);
+void GDebounce_Write_Output(uint count);
 //===============================================
 bit GDebounce_Read_Input(uchar debounce) {
     bit lState = SWITCH_RELEASED;
     if(SWITCH_PIN == 0) {
         GDelay_ms(debounce);
         if(SWITCH_PIN == 0) {
+            while(SWITCH_PIN == 0);
             lState = SWITCH_PRESSED;
         }
     }
     return lState;
 }
 //===============================================
-void GDebounce_Write_Output(bit state) {
-    if(state == SWITCH_PRESSED) {
-        OUTPUT_PORT = OUTPUT_PRESSED;
-    }
-    else {
-        OUTPUT_PORT = OUTPUT_RELEASED;
-    }
+void GDebounce_Write_Output(uint count) {
+    COUNTER_PORT = ~count;
 }
 //===============================================
 void GDebounce_Init() {
     SWITCH_PIN = 1;
-    OUTPUT_PORT = OUTPUT_RELEASED;
+    COUNTER_PORT = COUNTER_OFF;
 }
 //===============================================
 void GDebounce_Update() {
     gDebounce_State = GDebounce_Read_Input(30);
-    GDebounce_Write_Output(gDebounce_State);
+    if(gDebounce_State == SWITCH_PRESSED) gDebounce_Count++;
+    GDebounce_Write_Output(gDebounce_Count);
 }
 //===============================================
