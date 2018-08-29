3334e28222b785a438db54be366d64dc5ae23b7a
Gerard KESSE <tiakagerard@hotmail.com>
2018-08-29 12:31:38 +0200

    Initial Commit

---
+++ Tutoriels/Java/Cours/Ant/src/GMain.java
@@ -0,0 +1,7 @@
+//===============================================
+public class GMain {
+   public static void main(String[] args) {
+      System.out.println("Bonjour tout le monde");
+   }
+}
+//===============================================
---
+++ Tutoriels/Java/Cours/Ant/win/cmd_run.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set PATH=C:\Program Files\Java\jdk-10.0.2\bin
+set PATH=C:\Users\sabine\Downloads\Gerard\apache-ant-1.10.5\bin;%PATH%
+
+echo.
+ant
+echo.
