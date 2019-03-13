792f722cb350640a8ceb1877918d6818b63b59eb
Gerard KESSE <tiakagerard@hotmail.com>
2019-03-13 18:24:12 +0100

    Initial Commit

--- Tutoriels/Java/Utilisation/Javac/Jar_Executable_Manifest/win/cmd_compile.bat
+++ Tutoriels/Java/Utilisation/Javac/Jar_Executable_Manifest/win/cmd_compile.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set PATH=C:\Program Files (x86)\Java\jdk1.8.0_77\bin
+set PATH=C:\Program Files\Java\jdk-10.0.2\bin
 
 set GSRC=..\src
 set GBUILD=build
-set GSRCPATH=%GSRC%
+set GSRCPATH=..\src
 
 javac %GSRC%\GMain.java ^
 -d %GBUILD% ^
