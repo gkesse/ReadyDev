e0f559bf0d80d1a3f901a5d8eb6dd209773c52e0
Gerard KESSE <tiakagerard@hotmail.com>
2019-07-14 23:28:46 +0200

    Initial Commit

--- Admin/Formula/index.php
+++ Admin/Formula/index.php
@@ -2,7 +2,12 @@
 <html>
     <head>
         <title>ReadyFormula</title>
-        <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+        <script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+        <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({
+            tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
+        });
+        </script>
         <style>
             html, body {
                 border: 0px;
