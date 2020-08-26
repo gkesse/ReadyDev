278e460d87469cf641e1d3058825bb0ff3b6b99f
Gerard KESSE <tiakagerard@hotmail.com>
2020-08-26 14:08:03 +0200

    Initial Commit

---
+++ Cours/Electronics/Voltage_Divider/index.php
@@ -0,0 +1,22 @@
+<?php
+    require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
+    
+    $m_data = GJson::Instance()->getData("data/json/Electronics_Voltage_Divider.json");
+    
+    GConfig::Instance()->setData("title", "Pont diviseur de tension");
+    GConfig::Instance()->setData("menu", "Cours");
+    GConfig::Instance()->setData("view", "Electronics_Voltage_Divider");
+    GConfig::Instance()->setData("link", $m_data["links"]);
+
+    require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
+?>
+<!-- ============================================ -->
+<div class="MainBody">
+    <!-- ============================================ -->
+    <?php require "page/main.php"; ?>
+    <!-- ============================================ -->
+</div>
+<!-- ============================================ -->
+<?php require $_SERVER["DOCUMENT_ROOT"]."/php/footer.php"; ?>
+<!-- ============================================ -->
+
