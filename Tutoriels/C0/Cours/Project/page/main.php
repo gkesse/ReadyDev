d7bbe1e4002c97b60132eed5b650e01ccb547039
Gerard KESSE <tiakagerard@hotmail.com>
2020-09-26 13:24:57 +0200

    Initial Commit

--- Tutoriels/C0/Cours/Project/page/main.php
+++ Tutoriels/C0/Cours/Project/page/main.php
@@ -1,8 +1,48 @@
-<div class="Parallax GParallax1"><div class="Img Binary"><div class="Caption"><a href="#"><div class="Text">Créer un projet en C</div></a></div></div><div class="Body2 Orange">Le but de ce tutoriel est de vous apprendre à créer un projet en <b>C</b>.<br>Version Pro &amp; Industrielle. Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary1"><div class="Body0" id="Loader_1529412976174"><div class="Row26">Summary 1</div></div><script>loadSummary1("Loader_1529412976174");</script></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Introduction"><a class="Link3" href="#">Introduction</a></h1><div class="Body3"><b>C </b>est un langage de programmation impératif généraliste, de bas niveau offrant au développeur une marge de contrôle importante sur la machine (notamment sur la gestion de la mémoire) et est de ce fait utilisé pour réaliser les fondations (compilateurs, interpréteurs) des langages plus modernes. <br><br><div class="Img3 GImage"><img src="/Tutoriels/C0/img/b_cours_project.png" alt="/Tutoriels/C0/img/b_cours_project.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Créer un projet C"><a class="Link3" href="#">Créer un projet C</a></h1><div class="Body3"><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1599250744051"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1599250744051");</script></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Introduction"><a class="Link9" href="#Créer un projet C">Introduction</a></h2><br>Un projet C commence par un point d'entrée défini par la fonction (main) qui correspond au programme principal.<br><br>Dans l'exemple qui suit, nous essayons d'afficher un message à l'écran (Bonjour tout le monde).<br><br><h2 class="Title7 GTitle2" id="Créer un projet C-Programme principal"><a class="Link9" href="#Créer un projet C">Programme principal</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
+<div class="Parallax GParallax1"><div class="Img Binary"><div class="Caption"><a href="#"><div class="Text">Créer un projet en C</div></a></div></div><div class="Body2 Orange">Le but de ce tutoriel est de vous apprendre à créer un projet en <b>C</b>.<br>Version Pro &amp; Industrielle. Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary1"><div class="Body0" id="Loader_1529412976174"><div class="Row26">Summary 1</div></div><script>loadSummary1("Loader_1529412976174");</script></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Introduction"><a class="Link3" href="#">Introduction</a></h1><div class="Body3"><b>C </b>est un langage de programmation impératif généraliste, de bas niveau offrant au développeur une marge de contrôle importante sur la machine (notamment sur la gestion de la mémoire) et est de ce fait utilisé pour réaliser les fondations (compilateurs, interpréteurs) des langages plus modernes. <br><br><div class="Img3 GImage"><img src="/Tutoriels/C0/img/b_cours_project.png" alt="/Tutoriels/C0/img/b_cours_project.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Créer un projet C"><a class="Link3" href="#">Créer un projet C</a></h1><div class="Body3"><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1599250744051"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1599250744051");</script></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Introduction"><a class="Link9" href="#Créer un projet C">Introduction</a></h2><br>Dans l'exemple qui suit, nous essayons de créer un projet qui affiche un message à l'écran (Bonjour tout le monde).<br><br><h2 class="Title7 GTitle2" id="Créer un projet C-Programme principal"><a class="Link9" href="#Créer un projet C">Programme principal</a></h2><br>Pour la définition du programme principal. Le message (Bonjour tout le monde) est affiché par la fonction (printf) de la librairie (stdio.h).<br><br><h3 class="Title8 GTitle3">main.c</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
 #include <stdio.h>
 //===============================================
 int main(int argc, char** argv) {
     printf("Bonjour tout le monde\n");
     return 0:
 }
-//===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Résultat"><a class="Link9" href="#Créer un projet C">Résultat</a></h2><br><div class="Img3 GImage"><img src="/Tutoriels/C0/img/i_cours_project.png" alt="/Tutoriels/C0/img/i_cours_project.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Voir Aussi"><a class="Link3" href="#">Voir Aussi</a></h1><div class="Body3">Dans cette même catégorie, vous pouvez consulter aussi :<br><br><span class="GColor1" style="color:lime;">Programmation de base</span><br><br><div class="Content0 GList1"><div class="Body0" id="Loader_1594465340717"><div class="Row26">List 1 &gt; C &gt; basics</div></div><script>loadList1("Loader_1594465340717","C","basics");</script></div><br></div></div></div></div><br>
+//===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Fichier Makefile"><a class="Link9" href="#Créer un projet C">Fichier Makefile</a></h2><br>Pour la définition du fichier Makefile. Le programme principal est situé dans le répertoire des fichiers sources (src). L'exécutable du projet est nommé (gp_c) et est généré dans le répertoire des fichiers binaires (bin). Les fichiers intermédiaires sont générés dans le répertoire des fichiers objet (build). On décide d'utiliser le standard C (gnu18), d'afficher tous les warnings (-W -Wall) sauf les warnings du type (-Wno-unused-parameter). La compilation et l'édition de liens est réalisé par la commande (gcc). Le nettoyage du projet est réalisé par la commande (del).<br><br><h3 class="Title8 GTitle3">Makefile (Windows - MinGW)</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="makefile">GSRC = src
+GBIN = bin
+GBUILD = build
+GTARGET = $(GBIN)\gp_c.exe
+
+GINCS =\
+    
+GLIBS =\
+    
+GOBJS =\
+    $(patsubst $(GSRC)/%.c, $(GBUILD)/%.o, $(wildcard $(GSRC)/*.c)) \
+
+GCFLAGS = \
+    -std=gnu18 \
+    -W -Wall \
+    -Wno-unused-parameter \
+
+all: clean compile
+
+compile: $(GOBJS)
+	@if not exist "$(GBIN)" @mkdir $(GBIN)
+	gcc -o $(GOBJS) $(GLIBS) 
+$(GBUILD)/%.o: $(GSRC)/%.c
+	@if not exist "$(GBUILD)" @mkdir $(GBUILD)
+	gcc $(GCFLAGS) -c $&lt; -o $@ $(GINCS)
+clean_exe:
+	@if not exist "$(GBIN)" @mkdir $(GBIN)
+	del /q $(GBIN)\*
+clean:
+	@if not exist "$(GBIN)" @mkdir $(GBIN)
+	@if not exist "$(GBUILD)" @mkdir $(GBUILD)
+	del /q $(GBUILD)\* $(GBIN)\*</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Compilation automatique"><a class="Link9" href="#Créer un projet C">Compilation automatique</a></h2><br>Pour la compilation automatique du projet. On positionne les variables d'environnement (PATH) associées à l'utilitaire (MinGW). On compile le projet par la commande (ming32-make). On lance l'exécutable par la commande (bin\gp_c).<br><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">@echo off
+::===============================================
+set "PATH=C:\MinGW\bin;%PATH%"
+::===============================================
+mingw32-make
+bin\gp_c
+::===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Compilation manuelle"><a class="Link9" href="#Créer un projet C">Compilation manuelle</a></h2><br>Pour la compilation manuelle du projet. On nettoie le projet par la commande (del). On compile le projet par la 1ère commande (gcc). On lance l'édition des liens du projet par la 2ème commande (gcc). On lance l'exécutable par la commande (bin\gp_c)<br><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">del /q build\* bin\*
+gcc -std=gnu18 -W -Wall -Wno-unused-parameter  -c src/main.c -o build/main.o
+gcc -o bin\gp_c.exe  build/main.o
+bin\gp_c</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet C-Résultat"><a class="Link9" href="#Créer un projet C">Résultat</a></h2><br><div class="Img3 GImage"><img src="/Tutoriels/C0/img/i_cours_project.png" alt="/Tutoriels/C0/img/i_cours_project.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Voir Aussi"><a class="Link3" href="#">Voir Aussi</a></h1><div class="Body3">Dans cette même catégorie, vous pouvez consulter aussi :<br><br><span class="GColor1" style="color:lime;">Programmation de base</span><br><br><div class="Content0 GList1"><div class="Body0" id="Loader_1594465340717"><div class="Row26">List 1 &gt; C &gt; basics</div></div><script>loadList1("Loader_1594465340717","C","basics");</script></div><br></div></div></div></div><br>
