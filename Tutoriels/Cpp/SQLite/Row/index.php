5c6698efd296864b5bd1fa0e89d4d61dbcb71959
Gerard KESSE <tiakagerard@hotmail.com>
2020-10-07 05:44:49 +0200

    Initial Commit

--- Tutoriels/Cpp/SQLite/Row/index.php
+++ Tutoriels/Cpp/SQLite/Row/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Write.json");
+    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Row.json");
     
-    GConfig::Instance()->setData("title", "C++ - SQLite - Ecrire des données");
+    GConfig::Instance()->setData("title", "C++ - SQLite - Lire une ligne de données");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Cpp_SQLite_Write");
+    GConfig::Instance()->setData("view", "Cpp_SQLite_Row");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
