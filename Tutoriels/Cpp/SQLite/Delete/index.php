d0864382bbaa4bd2aadcc66da8360ba4344e58cf
Gerard KESSE <tiakagerard@hotmail.com>
2020-10-07 19:48:34 +0200

    Initial Commit

--- Tutoriels/Cpp/SQLite/Delete/index.php
+++ Tutoriels/Cpp/SQLite/Delete/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Insert.json");
+    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Delete.json");
     
-    GConfig::Instance()->setData("title", "C++ - SQLite - Insérer des données dans une table");
+    GConfig::Instance()->setData("title", "C++ - SQLite - Supprimer des données dans une table");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Cpp_SQLite_Insert");
+    GConfig::Instance()->setData("view", "Cpp_SQLite_Delete");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
--- Tutoriels/Cpp/SQLite/Update/index.php
+++ Tutoriels/Cpp/SQLite/Update/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Insert.json");
+    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Update.json");
     
-    GConfig::Instance()->setData("title", "C++ - SQLite - Insérer des données dans une table");
+    GConfig::Instance()->setData("title", "C++ - SQLite - Modifier des données dans une table");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Cpp_SQLite_Insert");
+    GConfig::Instance()->setData("view", "Cpp_SQLite_Update");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
