93afbe9af0c59636886cfffc4818572f1c40120e
Gerard KESSE <tiakagerard@hotmail.com>
2020-10-20 06:51:55 +0200

    Initial Commit

--- Tutoriels/C0/Cours/Strategy/index.php
+++ Tutoriels/C0/Cours/Strategy/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/C_Cours_Singleton.json");
+    $m_data = GJson::Instance()->getData("data/json/C_Cours_Strategy.json");
     
     GConfig::Instance()->setData("title", "C - Créer un patron stratégie");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "C_Cours_Singleton");
+    GConfig::Instance()->setData("view", "C_Cours_Strategy");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
