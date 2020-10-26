d8dac8064e7614e1567b785a38dbe35208dcaf09
Gerard KESSE <tiakagerard@hotmail.com>
2020-10-26 07:27:53 +0100

    Initial Commit

--- Tutoriels/Go/Cours/State_Machine/index.php
+++ Tutoriels/Go/Cours/State_Machine/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Go_Cours_Project.json");
+    $m_data = GJson::Instance()->getData("data/json/Go_Cours_State_Machine.json");
     
     GConfig::Instance()->setData("title", "Go - Créer une machine à états finis");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Go_Cours_Project");
+    GConfig::Instance()->setData("view", "Go_Cours_State_Machine");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
