c777c2ee9d8a00105cbfbd282468227a12154365
Gerard KESSE <tiakagerard@hotmail.com>
2020-11-10 17:47:03 +0100

    Initial Commit

---
+++ Tutoriels/Go/Wrapper/Swig_Cpp_Function/page/main.php
@@ -0,0 +1,72 @@
+<div class="Parallax GParallax1"><div class="Img Binary"><div class="Caption"><a href="#"><div class="Text">Utiliser une fonction C++ avec swig en Go</div></a></div></div><div class="Body2 Orange">Le but de ce tutoriel est de vous apprendre à utiliser une fonction C++ avec swig en <b>Go</b>.<br>Version Pro &amp; Industrielle. Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary1"><div class="Body0" id="Loader_1529412976174"><div class="Row26">Summary 1</div></div><script>loadSummary1("Loader_1529412976174");</script></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Introduction"><a class="Link3" href="#">Introduction</a></h1><div class="Body3"><b>Go </b>est un langage de programmation compilé et concurrent inspiré de C et Pascal. Il veut faciliter et accélérer la programmation à grande échelle en raison de sa simplicité. Il peut être utilisé aussi bien pour écrire des applications, des scripts ou de grands systèmes. Il permet d'assurer la maintenance et l'évolution des programmes sur plusieurs générations de développeurs.<br><br><div class="Img3 GImage"><img src="/Tutoriels/Go/img/b_wrapper_swig_cpp_function.png" alt="/Tutoriels/Go/img/b_wrapper_swig_cpp_function.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Générer une interface Go avec swig"><a class="Link3" href="#">Générer une interface Go avec swig</a></h1><div class="Body3"><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1599250744051"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1599250744051");</script></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Introduction"><a class="Link9" href="#Générer une interface Go avec swig">Introduction</a></h2><br>Dans l'exemple qui suit, nous essayons de générer une interface Go avec swig à partir de fichier C++.<br><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Déclaration de la librairie"><a class="Link9" href="#Générer une interface Go avec swig">Déclaration de la librairie</a></h2><br><h3 class="Title8 GTitle3">hello.h</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
+#ifndef _hello_
+#define _hello_
+//===============================================
+void sayHello();
+//===============================================
+#endif
+//===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Définition de la librairie"><a class="Link9" href="#Générer une interface Go avec swig">Définition de la librairie</a></h2><br><h3 class="Title8 GTitle3">hello.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
+#include "hello.h"
+//===============================================
+#include &lt;iostream&gt;
+//===============================================
+void sayHello() {
+    std::cout &lt;&lt; "[info] integration d'une fonction C++ avec swig en Go\n";
+}
+//===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Définition de linterface swig"><a class="Link9" href="#Générer une interface Go avec swig">Définition de linterface swig</a></h2><br><h3 class="Title8 GTitle3">hello.i</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">%module hello 
+
+%inline %{ 
+    #include "hello.h" 
+%}
+
+void sayHello();</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Fichier Makefile"><a class="Link9" href="#Générer une interface Go avec swig">Fichier Makefile</a></h2><br><h3 class="Title8 GTitle3">Makefile (MinGW - Windows)<br></h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="makefile">GINTERFACE =\
+    $(wildcard *.i) \
+
+GFILES =\
+    $(patsubst %.i, %.go, $(wildcard *.i)) \
+    $(patsubst %.i, %_wrap.cxx, $(wildcard *.i)) \
+
+all: clean compile
+
+compile: $(GINTERFACE)
+	swig -go -cgo -c++ -intgosize 64 $(GINTERFACE)
+clean: 
+	del /q $(GFILES)</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Variables d'environnement"><a class="Link9" href="#Générer une interface Go avec swig">Variables d'environnement</a></h2><br><h3 class="Title8 GTitle3">env.bat (Windows)<br></h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">@echo off
+::===============================================
+set "PATH=C:\TDM-GCC-64\bin;%PATH%"
+set "PATH=C:\Users\Admin\Downloads\Compressed\swigwin-4.0.2\swigwin-4.0.2;%PATH%"
+::===============================================
+set "GOPATH=C:\Users\Admin\Downloads\Programs\ReadyTest\go\wrapper\p02"
+::===============================================</xmp></pre></div><br><h3 class="Title8 GTitle3">make.bat (MinGW - Windows)</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">@echo off
+::===============================================
+mingw32-make %*
+::===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Compilation automatique"><a class="Link9" href="#Générer une interface Go avec swig">Compilation automatique</a></h2><br><h3 class="Title8 GTitle3">Terminal</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">make</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Compilation manuelle"><a class="Link9" href="#Générer une interface Go avec swig">Compilation manuelle</a></h2><br><h3 class="Title8 GTitle3">Terminal</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">swig -go -cgo -c++ -intgosize 64 hello.i</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Générer une interface Go avec swig-Résultat"><a class="Link9" href="#Générer une interface Go avec swig">Résultat</a></h2><br><div class="Img3 GImage"><img src="/Tutoriels/Go/img/i_wrapper_swig_cpp_function.png" alt="/Tutoriels/Go/img/i_wrapper_swig_cpp_function.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Utiliser une fonction C++ avec swig en Go"><a class="Link3" href="#">Utiliser une fonction C++ avec swig en Go</a></h1><div class="Body3"><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1605020825927"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1605020825927");</script></div><br><h2 class="Title7 GTitle2" id="Utiliser une fonction C++ avec swig en Go-Introduction"><a class="Link9" href="#Utiliser une fonction C++ avec swig en Go">Introduction</a></h2><br>Dans l'exemple qui suit, nous essayons d'utiliser une fonction C++ avec swig en Go.<br><br><h2 class="Title7 GTitle2" id="Utiliser une fonction C++ avec swig en Go-Programme principal"><a class="Link9" href="#Utiliser une fonction C++ avec swig en Go">Programme principal</a></h2><br><h3 class="Title8 GTitle3">main.go</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="golang">//===============================================
+package main
+//===============================================
+import "lib"
+import "fmt"
+//===============================================
+func main() {
+    fmt.Printf("[info] lancement du programme Go\n")
+    hello.SayHello()
+}
+//===============================================</xmp></pre></div><br><h3 class="Title8 GTitle3">lib.go</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="golang">//===============================================
+package hello
+//===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Utiliser une fonction C++ avec swig en Go-Fichier Makefile"><a class="Link9" href="#Utiliser une fonction C++ avec swig en Go">Fichier Makefile</a></h2><br><h3 class="Title8 GTitle3">Makefile</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="makefile">GSRC = ..\src
+GBIN = bin
+GTARGET = $(GBIN)\gp_go.exe
+    
+all: lib clean compile run
+
+compile:
+	@if not exist $(GBIN) ( @mkdir $(GBIN) )
+	go build -o $(GTARGET) $(GSRC)\main.go
+run: 
+	@echo.
+	@$(GTARGET)
+	@echo.
+clean: 
+	@if not exist $(GBIN) ( @mkdir $(GBIN) )
+	del /q $(GBIN)\*
+lib:
+	@make -C $(GSRC)/lib</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Utiliser une fonction C++ avec swig en Go-Résultat"><a class="Link9" href="#Utiliser une fonction C++ avec swig en Go">Résultat</a></h2><br><div class="Img3 GImage"><img src="/Tutoriels/Go/img/i_wrapper_swig_cpp_function_02.png" alt="/Tutoriels/Go/img/i_wrapper_swig_cpp_function_02.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Voir Aussi"><a class="Link3" href="#">Voir Aussi</a></h1><div class="Body3">Dans cette même catégorie, vous pouvez consulter aussi :<br><br><span class="GColor1" style="color:lime;">Développement de wrapper</span><br><br><div class="Content0 GList1"><div class="Body0" id="Loader_1604568280960"><div class="Row26">List 1 &gt; Go &gt; wrapper</div></div><script>loadList1("Loader_1604568280960","Go","wrapper");</script></div><br></div></div></div></div><br>
