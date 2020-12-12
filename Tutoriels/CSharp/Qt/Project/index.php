a61511aacef150a4d0bc30989edc7a79d74e5b4a
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-12 04:37:08 +0100

    Initial Commit

--- Tutoriels/CSharp/Qt/Project/index.php
+++ Tutoriels/CSharp/Qt/Project/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/CSharp_Cours_Project.json");
+    $m_data = GJson::Instance()->getData("data/json/CSharp_Qt_Project.json");
     
-    GConfig::Instance()->setData("title", "C# - Créer un projet");
+    GConfig::Instance()->setData("title", "C# - Qt - Créer un projet");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "CSharp_Cours_Project");
+    GConfig::Instance()->setData("view", "CSharp_Qt_Project");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
