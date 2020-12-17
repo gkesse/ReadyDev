6a4be82dad4d29d0cbf3772936bfaae5ed4e1f5e
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-17 01:29:10 +0100

    Initial Commit

--- Tutoriels/J2EE/Cours/Project/page/main.php
+++ Tutoriels/J2EE/Cours/Project/page/main.php
@@ -31,7 +31,7 @@ public class GHome extends HttpServlet {
     &lt;/body&gt;
     &lt;!-- ============================================ --&gt;
 &lt;/html&gt;
-&lt;!-- ============================================ --&gt;</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Fichier Makefile"><a class="Link9" href="#Créer un projet en J2EE">Fichier Makefile</a></h2><br><h3 class="Title8 GTitle3">Makefile</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="makefile">GSRC = $(GPROJECT_SRC)
+&lt;!-- ============================================ --&gt;</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Configuration du projet"><a class="Link9" href="#Créer un projet en J2EE">Configuration du projet</a></h2><br><h3 class="Title8 GTitle3">web.xml</h3><br><br><br><br><br><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Fichier Makefile"><a class="Link9" href="#Créer un projet en J2EE">Fichier Makefile</a></h2><br><h3 class="Title8 GTitle3">Makefile.jdk.mak</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="makefile">GSRC = $(GPROJECT_SRC)
 GBUILD = $(GPROJECT_CLASS)
 
 GSRCPATH =\
@@ -57,4 +57,28 @@ set "GMAIN_JAVA=%GPROJECT_SRC%\com\pkg\readyapp\GHome.java"
 ::===============================================
 set "CLASSPATH="
 set "CLASSPATH=C:\Users\Admin\Downloads\Compressed\apache-tomcat-7.0.107-windows-x64\apache-tomcat-7.0.107\lib\servlet-api.jar;%CLASSPATH%"
-::===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Résultat"><a class="Link9" href="#Créer un projet en J2EE">Résultat</a></h2><br><div class="Img3 GImage"><img src="/Tutoriels/J2EE/img/i_cours_project.png" alt="/Tutoriels/J2EE/img/i_cours_project.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Voir Aussi"><a class="Link3" href="#">Voir Aussi</a></h1><div class="Body3">Dans cette même catégorie, vous pouvez consulter aussi :<br><br><span class="GColor1" style="color:lime;">Programmation de base</span><br><br><div class="Content0 GList1"><div class="Body0" id="Loader_1600429735365"><div class="Row26">List 1 &gt; J2EE &gt; basics</div></div><script>loadList1("Loader_1600429735365","J2EE","basics");</script></div><br></div></div></div></div><br>
+::===============================================</xmp></pre></div><br><h3 class="Title8 GTitle3">make_j.bat</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">@echo off
+::===============================================
+set "args=%*"             
+setlocal enabledelayedexpansion
+set "lArgs=%*"             
+set "lArgs=!lArgs:*%1 =!"  
+endlocal && ( set "args=%lArgs%" )
+::===============================================
+mingw32-make -f Makefile.jdk.mak %1 argv="%args%"
+::===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Compilation automatique"><a class="Link9" href="#Créer un projet en J2EE">Compilation automatique</a></h2><br><h3 class="Title8 GTitle3">Terminal</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">make_j compile</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Compilation manuelle"><a class="Link9" href="#Créer un projet en J2EE">Compilation manuelle</a></h2><br><h3 class="Title8 GTitle3">Terminal</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="batchfile">javac %GPROJECT_SRC%\WEB-INF\sources\com\pkg\readyapp\GHome.java ^
+-d %GPROJECT_SRC%\WEB-INF\classes ^
+-sourcepath %GPROJECT_SRC%\WEB-INF\sources</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Configuration du serveur"><a class="Link9" href="#Créer un projet en J2EE">Configuration du serveur</a></h2><br><h3 class="Title8 GTitle3">server.xml</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="xml">&lt;!-- ============================================ --&gt;
+&lt;Service name="Catalina_2"&gt;
+    &lt;Connector port="8001" protocol="HTTP/1.1" connectionTimeout="20000" redirectPort="8444" /&gt;
+    &lt;Engine name="Catalina_2" defaultHost="localhost"&gt;
+        &lt;Realm className="org.apache.catalina.realm.LockOutRealm"&gt;
+            &lt;Realm className="org.apache.catalina.realm.UserDatabaseRealm" resourceName="UserDatabase"/&gt;
+        &lt;/Realm&gt;
+        &lt;Host name="localhost"  appBase="C:\Users\Admin\Downloads\Programs\ReadyJ2EE\code" unpackWARs="true" autoDeploy="true"&gt;
+            &lt;Valve className="org.apache.catalina.valves.AccessLogValve" directory="logs" prefix="localhost_access_log." suffix=".txt" pattern="%h %l %u %t "%r" %s %b" /&gt;
+            &lt;Context docBase="webapp/src" path="" reloadable="true"/&gt;            
+        &lt;/Host&gt;
+    &lt;/Engine&gt;
+&lt;/Service&gt;
+&lt;!-- ============================================ --&gt;</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Lancement du serveur"><a class="Link9" href="#Créer un projet en J2EE">Lancement du serveur</a></h2><br><h3 class="Title8 GTitle3">Tomcat</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="txt">Tomcat7w.exe</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Accès à la page web"><a class="Link9" href="#Créer un projet en J2EE">Accès à la page web</a></h2><br><h3 class="Title8 GTitle3">Navigateur</h3><br><a class="Link7 GLink1" style="color:lime;" target="_blank" href="http://localhost:8001">http://localhost:8001</a><br><br><h2 class="Title7 GTitle2" id="Créer un projet en J2EE-Résultat"><a class="Link9" href="#Créer un projet en J2EE">Résultat</a></h2><br><h3 class="Title8 GTitle3">Structure du projet</h3><br><div class="Img3 GImage"><img src="/Tutoriels/J2EE/img/i_cours_project.png" alt="/Tutoriels/J2EE/img/i_cours_project.png"></div><br><h3 class="Title8 GTitle3">Page web</h3><br><div class="Img3 GImage"><img src="/Tutoriels/J2EE/img/i_cours_project_02.png" alt="/Tutoriels/J2EE/img/i_cours_project_02.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Voir Aussi"><a class="Link3" href="#">Voir Aussi</a></h1><div class="Body3">Dans cette même catégorie, vous pouvez consulter aussi :<br><br><span class="GColor1" style="color:lime;">Programmation de base</span><br><br><div class="Content0 GList1"><div class="Body0" id="Loader_1600429735365"><div class="Row26">List 1 &gt; J2EE &gt; basics</div></div><script>loadList1("Loader_1600429735365","J2EE","basics");</script></div><br></div></div></div></div><br>
