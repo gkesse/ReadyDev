e7aa00e1b77abfbe864f616e5a9982dc02b1f40d
Gerard KESSE <tiakagerard@hotmail.com>
2021-01-21 14:33:25 +0100

    Initial Commit

--- Tutoriels/Cpp/Cours/Admin_System/index.php
+++ Tutoriels/Cpp/Cours/Admin_System/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Cpp_Cours_Cross_Plateform.json");
+    $m_data = GJson::Instance()->getData("data/json/Cpp_Cours_Admin_System.json");
     
-    GConfig::Instance()->setData("title", "C++ - Créer un projet multiplateforme");
+    GConfig::Instance()->setData("title", "C++ - Créer un système d'administration");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Cpp_Cours_Cross_Plateform");
+    GConfig::Instance()->setData("view", "Cpp_Cours_Admin_System");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
--- Tutoriels/Cpp/Cours/Config/page/main.php
+++ Tutoriels/Cpp/Cours/Config/page/main.php
@@ -2,58 +2,58 @@
 GConfig::Instance()-&gt;setData("CPP_ADMIN_ID", lAnswer);
 GConfig::Instance()-&gt;saveData("CPP_ADMIN_ID");
 GConfig::Instance()-&gt;loadData("CPP_ADMIN_ID");</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Dictionnaire de configurations"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Dictionnaire de configurations</a></h2><br><h3 class="Title8 GTitle3">GConfig.h</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">std::map&lt;std::string, std::string&gt; m_dataMap;</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Ajouter une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Ajouter une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-void GConfig::setData(std::string keyIn, std::string valueIn) {
-    m_dataMap[keyIn] = valueIn;
+void GConfig::setData(std::string key, std::string value) {
+    m_dataMap[key] = value;
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Lire une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Lire une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-std::string GConfig::getData(std::string keyIn) {
-    return m_dataMap[keyIn];
+std::string GConfig::getData(std::string key) {
+    return m_dataMap[key];
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Sauvegarder une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Sauvegarder une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-void GConfig::saveData(std::string keyIn, std::string valueIn) {
-    if(valueIn == "") valueIn = m_dataMap[keyIn];
-    if(checkData(keyIn) == 0) {insertData(keyIn, valueIn);}
-    else {updateData(keyIn, valueIn);}
+void GConfig::saveData(std::string key, std::string value) {
+    if(value == "") value = m_dataMap[key];
+    if(countData(key) == 0) {insertData(key, value);}
+    else {updateData(key, value);}
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Charger une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Charger une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-void GConfig::loadData(std::string keyIn) {
+void GConfig::loadData(std::string key) {
     char lSql[B_QUERY+1];
     sprintf(lSql, ""
     "select CONFIG_VALUE\n"
     "from CONFIG_DATA\n"
     "where CONFIG_KEY='%s'\n"
-    "", keyIn.c_str());
+    "", key.c_str());
     std::string lData = GSQLiteMgr::Instance()-&gt;queryValue("", lSql);
-    m_dataMap[keyIn] = lData;
+    m_dataMap[key] = lData;
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Vérifier l'existance d'une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Vérifier l'existance d'une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-int GConfig::checkData(std::string keyIn) {
+int GConfig::countData(std::string key) {
     char lSql[B_QUERY+1];
     sprintf(lSql, ""
     "select count(*)\n"
     "from CONFIG_DATA\n"
     "where CONFIG_KEY='%s'\n"
-    "", keyIn.c_str());
+    "", key.c_str());
     std::string lCount = GSQLiteMgr::Instance()-&gt;queryValue("", lSql);
     return std::stoi(lCount);
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Insérer une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Insérer une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-void GConfig::insertData(std::string keyIn, std::string valueIn) {
+void GConfig::insertData(std::string key, std::string value) {
     char lSql[B_QUERY+1];
     sprintf(lSql, ""
     "insert into CONFIG_DATA(CONFIG_KEY, CONFIG_VALUE)\n"
     "values('%s', '%s')\n"
-    "", keyIn.c_str(), valueIn.c_str());
+    "", key.c_str(), value.c_str());
     GSQLiteMgr::Instance()-&gt;queryWrite("", lSql);
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Modifier une configuration"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Modifier une configuration</a></h2><br><h3 class="Title8 GTitle3">GConfig.cpp</h3><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="c_cpp">//===============================================
-void GConfig::updateData(std::string keyIn, std::string valueIn) {
+void GConfig::updateData(std::string key, std::string value) {
     char lSql[B_QUERY+1];
     sprintf(lSql, ""
     "update CONFIG_DATA\n"
     "set CONFIG_VALUE='%s'\n"
     "where CONFIG_KEY='%s'\n"
-    "", valueIn.c_str(), keyIn.c_str());
+    "", value.c_str(), key.c_str());
     GSQLiteMgr::Instance()-&gt;queryWrite("", lSql);
 }
 //===============================================</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Créer un gestionnaire de configurations C++-Résultat"><a class="Link9" href="#Créer un gestionnaire de configurations C++">Résultat</a></h2><br><div class="Img3 GImage"><img src="/Tutoriels/Cpp/img/i_cours_config.png" alt="/Tutoriels/Cpp/img/i_cours_config.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Voir Aussi"><a class="Link3" href="#">Voir Aussi</a></h1><div class="Body3">Dans cette même catégorie, vous pouvez consulter aussi :<br><br><span class="GColor1" style="color:lime;">Programmation de base</span><br><br><div class="Content0 GList1"><div class="Body0" id="Loader_1600474250946"><div class="Row26">List 1 &gt; Cpp &gt; basics</div></div><script>loadList1("Loader_1600474250946","Cpp","basics");</script></div><br></div></div></div></div><br>
