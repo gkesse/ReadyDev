32b22c1ba674bc1bde2c3345e90ebb719c878c04
Gerard KESSE <tiakagerard@hotmail.com>
2021-03-03 03:54:19 +0100

    Initial Commit

--- Tutoriels/Embedded_System/RaspberryPi/LED_C/index.php
+++ Tutoriels/Embedded_System/RaspberryPi/LED_C/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Embedded_System_RaspberryPi_Environment.json");
+    $m_data = GJson::Instance()->getData("data/json/Embedded_System_RaspberryPi_LED_C.json");
     
-    GConfig::Instance()->setData("title", "RaspberryPi - Configurer l'environnement");
+    GConfig::Instance()->setData("title", "RaspberryPi - C- Gérer une diode LED");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Embedded_System_RaspberryPi_Environment");
+    GConfig::Instance()->setData("view", "Embedded_System_RaspberryPi_LED_C");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
