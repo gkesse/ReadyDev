fc51f72c16d8756f6850ac15a98c719af9b83e6a
Gerard KESSE <tiakagerard@hotmail.com>
2021-03-06 20:49:51 +0100

    Initial Commit

--- Tutoriels/Embedded_System/RaspberryPi/Configurations/index.php
+++ Tutoriels/Embedded_System/RaspberryPi/Configurations/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Embedded_System_RaspberryPi_Configuration.json");
+    $m_data = GJson::Instance()->getData("data/json/Embedded_System_RaspberryPi_Configurations.json");
     
     GConfig::Instance()->setData("title", "RaspberryPi - Configurations");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Embedded_System_RaspberryPi_Configuration");
+    GConfig::Instance()->setData("view", "Embedded_System_RaspberryPi_Configurations");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
