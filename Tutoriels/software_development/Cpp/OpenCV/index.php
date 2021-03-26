3b6e49204f467d39cd7beddf14292df49f831e9b
Gerard KESSE <tiakagerard@hotmail.com>
2021-03-26 01:58:52 +0100

    Initial Commit

--- Tutoriels/software_development/Cpp/OpenCV/index.php
+++ Tutoriels/software_development/Cpp/OpenCV/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Cpp_SQLite_Value.json");
+    $m_data = GJson::Instance()->getData("data/json/Cpp_OpenCV.json");
     
-    GConfig::Instance()->setData("title", "C++ - SQLite - Lire une valeur de donnée");
+    GConfig::Instance()->setData("title", "C++ - OpenCV");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Cpp_SQLite_Value");
+    GConfig::Instance()->setData("view", "Cpp_OpenCV");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
