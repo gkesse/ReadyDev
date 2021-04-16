c032f238384b932f8aec578b6658af4049090c92
Gerard KESSE <tiakagerard@hotmail.com>
2021-04-16 20:46:05 +0200

    Initial Commit

--- Tutoriels/VB.NET/Cours/Console/src/GMain.vb
+++
@@ -1,9 +0,0 @@
-'================================================'
-Module GModule
-    Sub Main()
-        Dim CR As String = Environment.newLine
-        Console.Write("### Console" + CR + CR)
-        Console.Write("Bonjour tout le monde" + CR)
-    End Sub
-End Module
-'================================================'
