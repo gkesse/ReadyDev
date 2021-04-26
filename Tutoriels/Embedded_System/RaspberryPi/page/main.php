9a5438a733b5f7fa1fe20f88244f99f3cd5412a4
Gerard KESSE <tiakagerard@hotmail.com>
2021-04-26 20:27:52 +0200

    Initial Commit

--- Tutoriels/Embedded_System/RaspberryPi/page/main.php
+++ Tutoriels/Embedded_System/RaspberryPi/page/main.php
@@ -133,74 +133,50 @@ lsblk -p
 # on démonte la carte SD de la machine
 umount /dev/sda*
 # on grave l'image sur la carte SD
-sudo dd if=core-image-minimal-raspberrypi4.rpi-sdimg of=/dev/sda</xmp></pre></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Buildroot"><a class="Link3" href="#">Buildroot</a></h1><div class="Body3"><br><b>Buildroot </b>est un ensemble de Makefiles et de correctifs qui simplifie et automatise le processus de création d'un environnement Linux complet et amorçable pour un système embarqué , tout en utilisant la compilation croisée pour permettre la construction de plusieurs plates-formes cibles sur un seul système de développement basé sur Linux.<br><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1619395596885"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1619395596885");</script></div><br><h2 class="Title7 GTitle2" id="Buildroot-1 - Génération d'une image Linux embarqué"><a class="Link9" href="#Buildroot">1 - Génération d'une image Linux embarqué</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1 - Génération sous Ubuntu"><a class="Link9" href="#Buildroot">1.1 - Génération sous Ubuntu</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh"># on telecharge buildroot
-wget https://buildroot.org/downloads/buildroot-2021.02.1.tar.gz
-
-# on extraire buildroot
-tar xzfv buildroot-2021.02.1.tar.gz
-
-# on liste les machines supportees
+sudo dd if=core-image-minimal-raspberrypi4.rpi-sdimg of=/dev/sda</xmp></pre></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Buildroot"><a class="Link3" href="#">Buildroot</a></h1><div class="Body3"><br><b>Buildroot </b>est un ensemble de Makefiles et de correctifs qui simplifie et automatise le processus de création d'un environnement Linux complet et amorçable pour un système embarqué , tout en utilisant la compilation croisée pour permettre la construction de plusieurs plates-formes cibles sur un seul système de développement basé sur Linux.<br><br><div class="Content0 GSummary2"><div class="Body0" id="Loader_1619395596885"><div class="Row26">Summary 2</div></div><script>loadSummary2("Loader_1619395596885");</script></div><br><h2 class="Title7 GTitle2" id="Buildroot-1 - Génération d'une image Linux embarqué"><a class="Link9" href="#Buildroot">1 - Génération d'une image Linux embarqué</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1 - Génération sous Ubuntu"><a class="Link9" href="#Buildroot">1.1 - Génération sous Ubuntu</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.1 - Télécharger Buildroot"><a class="Link9" href="#Buildroot">1.1.1 - Télécharger Buildroot</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh"># on telecharge buildroot
+wget https://buildroot.org/downloads/buildroot-2021.02.1.tar.gz</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.2 - Extraire Buildroot"><a class="Link9" href="#Buildroot">1.1.2 - Extraire Buildroot</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh"># on extraire buildroot
+tar xzfv buildroot-2021.02.1.tar.gz</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.3 - Lister les machines"><a class="Link9" href="#Buildroot">1.1.3 - Lister les machines</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh"># on liste les machines supportees
 cd buildroot-2021.02.1
-ls -l configs
-
-# on genere la configuration par defaut pour le raspberrypi
-make raspberrypi4_64_defconfig
-
-# on personnalise la configuration pour le raspberrypi
-make menuconfig
-
-# Target Options
-Target Options -&gt; Target Architecture -&gt; AArch64 (Little Endian)
-Target Options -&gt; Target Binary Format -&gt; ELF
-Target Options -&gt; Target Architecture Variant -&gt; Cortex-A72
-Target Options -&gt; Floating Point Strategy -&gt; VFPv4
-
-# Build Options
-Build Options -&gt; Commands-&gt; Tar Options
-Build Options -&gt; Location to Save Buildroot Config-&gt; configs/raspberrypi4_64_defconfig
-Build Options -&gt; Download Dir -&gt; $(TOPDIR)/dl
-Build Options -&gt; Host Dir -&gt; $(TOPDIR)/host
-Build Options -&gt; Mirrors and Download Locations -&gt; Primary Download Site
-Build Options -&gt; Numbers of Jobs to Run Simultaneously (0 for Auto) -&gt; 0
-Build Options -&gt; Enable Compiler Cache -&gt; [ ]
-Build Options -&gt; Build Packages with Debugging Symbols -&gt; [ ]
-Build Options -&gt; Strip Target Binaries -&gt; [*]
-Build Options -&gt; Executables that Should not be Stripped -&gt; ( )
-Build Options -&gt; directories that should be skipped when stripping -&gt; ( )
-Build Options -&gt; gcc optimization level -&gt; optimize for size
-Build Options -&gt; libraries -&gt; shared only
-Build Options -&gt; location of a package override file-&gt; │$(CONFIG_DIR)/local.mk
-Build Options -&gt; global patch directories -&gt; ( )
-Build Options -&gt; Advanced -&gt; paranoid check of library/header paths -&gt;  [*]
-Build Options -&gt; Build code with PIC/PIE-&gt; [ ]
-Build Options -&gt; RELRO Protection -&gt; None
-
-# Toolchain
-Toolchain -&gt; Toolchain type-&gt; Buildroot toolchain
-Toolchain -&gt; custom toolchain vendor name -&gt; buildroot
-Toolchain -&gt;  C library -&gt; uClibc-ng
-Toolchain -&gt; Kernel Headers -&gt; Same as kernel being built 
-Toolchain -&gt; Custom kernel headers series -&gt; 5.10.x or later
-Toolchain -&gt; uClibc configuration file to use? -&gt; package/uclibc/uClibc-ng.config
-Toolchain -&gt; Additional uClibc configuration fragment files -&gt; ( )
-Toolchain -&gt; Enable WCHAR support -&gt; [ ]
-Toolchain -&gt; Enable toolchain locale/i18n support -&gt; [ ]
-Toolchain -&gt; Thread library implementation -&gt; Native POSIX Threading (NPTL)
-Toolchain -&gt; Thread library debugging -&gt; [ ]
-Toolchain -&gt; Enable stack protection support -&gt; [ ]
-Toolchain -&gt; Compile and install uClibc utilities -&gt; [*]
-Toolchain -&gt;  Binutils Version -&gt; binutils 2.35.2
-Toolchain -&gt; Additional binutils options -&gt; ( )
-Toolchain -&gt; GCC compiler Version -&gt;  gcc 9.x 
-Toolchain -&gt; Additional gcc options -&gt; ELF
-Toolchain -&gt;  Enable C++ support -&gt; [*]
-Toolchain -&gt; Enable Fortran support -&gt; [ ]
-Toolchain -&gt; Enable compiler link-time-optimization support -&gt; [ ]
-Toolchain -&gt; Enable compiler OpenMP support -&gt; [ ]
-Toolchain -&gt; Enable graphite support -&gt; [ ]
-Toolchain -&gt; Build cross gdb for the host -&gt; [ ]
-Toolchain -&gt; Extra toolchain libraries to be copied to target -&gt; ( )
-Toolchain -&gt; Enable MMU support -&gt; [*]
-Toolchain -&gt; Target Optimizations -&gt; ( )
-Toolchain -&gt; Target linker options -&gt; ( )
-Toolchain -&gt; Register toolchain within Eclipse Buildroot plug-in -&gt; [ ]</xmp></pre></div><br></div></div></div></div><br>
+ls -l configs</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.4 - Générer la configuration par défaut"><a class="Link9" href="#Buildroot">1.1.4 - Générer la configuration par défaut</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh"># on genere la configuration par defaut pour le raspberrypi
+make raspberrypi4_64_defconfig</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5 - Personnaliser la configuration"><a class="Link9" href="#Buildroot">1.1.5 - Personnaliser la configuration</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh"># on personnalise la configuration pour le raspberrypi
+make menuconfig</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.1 - Configuration du materiel"><a class="Link9" href="#Buildroot">1.1.5.1 - Configuration du materiel</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.1.1 - Configuration de l'architecture"><a class="Link9" href="#Buildroot">1.1.5.1.1 - Configuration de l'architecture</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Target options
+    Target Architecture
+        AArch64 (little endian)</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.1.2 - Configuration du fichier executable"><a class="Link9" href="#Buildroot">1.1.5.1.2 - Configuration du fichier executable</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Target options
+    Target Binary Format
+        ELF</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.1.3 - Configuration de la variante de processeur"><a class="Link9" href="#Buildroot">1.1.5.1.3 - Configuration de la variante de processeur</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Target options
+    Target Architecture Variant
+        cortex-A72</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.1.4 - Configuration du format de la virgule flottante"><a class="Link9" href="#Buildroot">1.1.5.1.4 - Configuration du format de la virgule flottante</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Target options
+    Floating point strategy
+        VFPv4</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.2 - Configuration des options de construction"><a class="Link9" href="#Buildroot">1.1.5.2 - Configuration des options de construction</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.2.1 - Configuration du fichier de sauvegarde de buildroot"><a class="Link9" href="#Buildroot">1.1.5.2.1 - Configuration du fichier de sauvegarde de buildroot</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Build options
+    Location to save buildroot config
+        buildroot-2021.02.1/configs/raspberrypi4_64_defconfig</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.2.2 - Configuration du repertoire de téléchargement"><a class="Link9" href="#Buildroot">1.1.5.2.2 - Configuration du repertoire de téléchargement</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Build options
+    Download dir
+        $(TOPDIR)/dl</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.2.3 - Configuration du repertoire machine"><a class="Link9" href="#Buildroot">1.1.5.2.3 - Configuration du repertoire machine</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Build options
+    Host dir
+        $(BASE_DIR)/host</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.2.4 - Configuration du cache de compilation"><a class="Link9" href="#Buildroot">1.1.5.2.4 - Configuration du cache de compilation</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Build options
+    [*] Enable compiler cache</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.3 - Configuration de la chaine de compilation"><a class="Link9" href="#Buildroot">1.1.5.3 - Configuration de la chaine de compilation</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.3.1 - Configuration du type de la chaine de compilation"><a class="Link9" href="#Buildroot">1.1.5.3.1 - Configuration du type de la chaine de compilation</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Toolchain
+    Toolchain type
+        Buildroot toolchain</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.3.2 - Configuration de la librairie C"><a class="Link9" href="#Buildroot">1.1.5.3.2 - Configuration de la librairie C</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Toolchain
+    C library
+        glibc</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.3.3 - Configuration du support C++"><a class="Link9" href="#Buildroot">1.1.5.3.3 - Configuration du support C++</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Toolchain
+     [*] Enable C++ support</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4- Configuration du système"><a class="Link9" href="#Buildroot">1.1.5.4- Configuration du système</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.1 - Configuration du nom de la machine"><a class="Link9" href="#Buildroot">1.1.5.4.1 - Configuration du nom de la machine</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+    System hostname
+        buildroot_rpi4</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.2 - Configuration du message d'accueil"><a class="Link9" href="#Buildroot">1.1.5.4.2 - Configuration du message d'accueil</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+    System banner
+        Welcome to Buildroot_RPI4</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.3 - Configuration du gestionnaire de commandes"><a class="Link9" href="#Buildroot">1.1.5.4.3 - Configuration du gestionnaire de commandes</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+Init system
+BusyBox</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.4 - Configuration du mot de passe root"><a class="Link9" href="#Buildroot">1.1.5.4.4 - Configuration du mot de passe root</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.4.1 - Activer le mot de passe root"><a class="Link9" href="#Buildroot">1.1.5.4.4.1 - Activer le mot de passe root</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+    [*] Enable root login with password</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.4.2 - Configurer le mot de passe root"><a class="Link9" href="#Buildroot">1.1.5.4.4.2 - Configurer le mot de passe root</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+    Root password
+        superadmin</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.5 - Configuration du shell par defaut"><a class="Link9" href="#Buildroot">1.1.5.4.5 - Configuration du shell par defaut</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.5.1 - Définir le gestionnaire de commandes"><a class="Link9" href="#Buildroot">1.1.5.4.5.1 - Définir le gestionnaire de commandes</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+Init system
+BusyBox</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.5.2 - Activer l'affichage du contenu du gestionnaire"><a class="Link9" href="#Buildroot">1.1.5.4.5.2 - Activer l'affichage du contenu du gestionnaire</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Target packages
+    [*]   Show packages that are also provided by busybox</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.5.3 - Sélectionner le shell par défaut"><a class="Link9" href="#Buildroot">1.1.5.4.5.3 - Sélectionner le shell par défaut</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+    /bin/sh
+        bash</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.4.6 - Configuration du timezone"><a class="Link9" href="#Buildroot">1.1.5.4.6 - Configuration du timezone</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">System configuration
+    [*] Install timezone info</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.5 - Configuration du noyau Linux"><a class="Link9" href="#Buildroot">1.1.5.5 - Configuration du noyau Linux</a></h2><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.5.1 - Activer le noyau Linux"><a class="Link9" href="#Buildroot">1.1.5.5.1 - Activer le noyau Linux</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Kernel
+    [*] Linux Kernel</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.5.2 - Définir la version du noyau Linux"><a class="Link9" href="#Buildroot">1.1.5.5.2 - Définir la version du noyau Linux</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Kernel
+    Kernel version
+        Custom Git repository</xmp></pre></div><br><h2 class="Title7 GTitle2" id="Buildroot-1.1.5.5.3 - Définir l'URL du noyau Linux"><a class="Link9" href="#Buildroot">1.1.5.5.3 - Définir l'URL du noyau Linux</a></h2><br><div class="GCode1"><pre class="Code2"><xmp class="AceCode" data-mode="sh">Kernel
+    URL of custom repository
+        https://github.com/raspberrypi/linux.git</xmp></pre></div><br></div></div></div></div><br>
