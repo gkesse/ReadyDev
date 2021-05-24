8d363a8a3e0d20c81f6b31d7b4d0fdcd70e40455
Gerard KESSE <tiakagerard@hotmail.com>
2021-05-24 19:31:46 +0200

    Initial Commit

--- php/class/GManager.php
+++ php/class/GManager.php
@@ -35,9 +35,9 @@ class GManager {
     public function getUrl($url) {
         $lUrl = $url;
         $lUrl = Normalizer::normalize($lUrl , Normalizer::FORM_D);
-        $lUrl = preg_replace('/[\x{0300}-\x{036f}]/u', "", $lUrl);
+        /*$lUrl = preg_replace('/[\x{0300}-\x{036f}]/u', "", $lUrl);
         $lUrl = preg_replace('/\s/u', "-", $lUrl);
-        /*$lUrl = str_replace(chr(34), "-", $lUrl);
+        $lUrl = str_replace(chr(34), "-", $lUrl);
         $lUrl = str_replace(chr(39), "-", $lUrl);*/
         return $lUrl;
     }
