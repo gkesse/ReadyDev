6e7cf990e73f9f0e4e52d1e266364744436fd20a
Gerard KESSE <tiakagerard@hotmail.com>
2021-05-30 22:19:27 +0200

    Initial Commit

--- js/class/GAce.js
+++ js/class/GAce.js
@@ -11,6 +11,63 @@ var GAce = (function() {
             },
             //===============================================
             setSyntax: function() {
+                var lAceCode = document.querySelectorAll(".AceCode");
+
+                var lObsOptions = {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: [0.0, 1.0]
+                };
+
+                var lObs = new IntersectionObserver(objs => {
+                    objs.forEach(obj => {
+                        if (obj.intersectionRatio > 0) {
+                            var lTarget = obj.target;
+                            if(!lTarget.hasAttribute("data-state")) {
+                                var lDataState = document.createAttribute("data-state");
+                                lDataState.value = "off";
+                                lTarget.setAttributeNode(lDataState);
+                            }
+                            if(lTarget.dataset.state == "on") {return;}
+                            // start lazy loading
+                            var lMode = lTarget.dataset.mode;
+                            var lHtml = lTarget.innerHTML;
+                            lTarget.innerHTML = lHtml;
+                            var lTheme = "gruvbox";
+                            var lEditor = ace.edit(lTarget);
+                            
+                            lTarget.style.backgroundColor = "transparent";
+                            lEditor.session.setMode("ace/mode/"+lMode);
+                            lEditor.renderer.$cursorLayer.element.style.display = "none";
+                            
+                            lEditor.setOptions({
+                                theme: "ace/theme/"+lTheme,
+                                maxLines: Infinity,
+                                readOnly: true,
+                                selectionStyle: "text",
+                                showLineNumbers: true,
+                                showGutter: true,
+                                displayIndentGuides: false,
+                                highlightActiveLine: false,
+                                showPrintMargin: false,
+                                showGutter: true,
+                                fontSize: "16px",
+                                highlightGutterLine: false,
+                                fontFamily: "courier",
+                                useWorker: false
+                            });                    
+                            // end lazy loading
+                            lTarget.dataset.state = "on";
+                        } 
+                    });
+                }, lObsOptions);
+
+                lAceCode.forEach(obj => {
+                    lObs.observe(obj);
+                });
+            },
+            //===============================================
+            setSyntax2: function() {
                 var lAceCode = document.getElementsByClassName("AceCode");
                 for(var i = 0; i < lAceCode.length; i++) {
                     this.setSyntaxNode(lAceCode[i]);                    
