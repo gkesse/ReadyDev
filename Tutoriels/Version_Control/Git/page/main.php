07746f3646f2e4a861600d4e27431143d9d8ea2c
Gerard KESSE <tiakagerard@hotmail.com>
2021-06-27 00:32:32 +0200

    Initial Commit

--- Tutoriels/Version_Control/Git/page/main.php
+++ Tutoriels/Version_Control/Git/page/main.php
@@ -1,4 +1,4 @@
-<div class="Parallax GParallax1"><div class="Img Binary"><div class="Caption"><a href="#"><div class="Text">Gestion de versions avec Git<br></div></a></div></div><div class="Body2 Orange"><br>Le but de ce tutoriel est de vous apprendre la gestion de verions avec <b>Git</b>.<br>Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary1"><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Introduction">Introduction</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Fondamentaux">Fondamentaux</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Installation">Installation</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Commandes">Commandes</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Indexation">Indexation</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Ignorance">Ignorance</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Historique">Historique</a></div></div><br></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Introduction"><a class="Link3" href="#">Introduction</a></h1><div class="Body3"><br><b>Git </b>est un outil de gestion de versions décentralisé.<br><br><div class="Img3 GImage"><img alt="/Tutoriels/Version_Control/Git/img/b_git.png" class="lazy" data-src="/Tutoriels/Version_Control/Git/img/b_git.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Fondamentaux"><a class="Link3" href="#">Fondamentaux</a></h1><div class="Body3"><br>Les fondamentaux de Git.<br><br><div class="Content0 GSummary2"><div class="Row26">Summary 2</div></div><br><h2 class="Title7 GTitle2" id="Fondamentaux-Installer-l-environnement-Git-sous-Windows"><a class="Link9" href="#Fondamentaux">Installer l'environnement Git sous Windows</a></h2><br><h3 class="Title8 GTitle3">Installer Git</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-mode="sh">sudo apt install git</pre></div></div><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-mode="sh">Lecture des listes de paquets... Fait
+<div class="Parallax GParallax1"><div class="Img Binary"><div class="Caption"><a href="#"><div class="Text">Gestion de versions avec Git<br></div></a></div></div><div class="Body2 Orange"><br>Le but de ce tutoriel est de vous apprendre la gestion de verions avec <b>Git</b>.<br>Produit par <b>Gérard KESSE</b>.<br><br><div class="Content0 GSummary1"><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Introduction">Introduction</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Fondamentaux">Fondamentaux</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Indexation">Indexation</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Ignorance">Ignorance</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Historique">Historique</a></div><div class="Item4"><span class="Icon10 fa fa-book"></span><a class="Link4" href="#Branches">Branches</a></div></div><br></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Introduction"><a class="Link3" href="#">Introduction</a></h1><div class="Body3"><br><b>Git </b>est un outil de gestion de versions décentralisé.<br><br><div class="Img3 GImage"><img src="/Tutoriels/Version_Control/Git/img/b_git.png" alt="/Tutoriels/Version_Control/Git/img/b_git.png"></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Fondamentaux"><a class="Link3" href="#">Fondamentaux</a></h1><div class="Body3"><br>Les fondamentaux de Git.<br><br><div class="Content0 GSummary2"><div class="Row26">Summary 2</div></div><br><h2 class="Title7 GTitle2" id="Fondamentaux-Installer-l-environnement-Git-sous-Windows"><a class="Link9" href="#Fondamentaux">Installer l'environnement Git sous Windows</a></h2><br><h3 class="Title8 GTitle3">Installer Git</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-mode="sh">sudo apt install git</pre></div></div><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-mode="sh">Lecture des listes de paquets... Fait
 Construction de l'arbre des dépendances
 Lecture des informations d'état... Fait
 ...
@@ -165,4 +165,123 @@ index 0000000..f72891d
 +++ b/README.md
 @@ -0,0 +1,2 @@
 +Apprendre à utiliser Git.
-+</pre></div></div><br></div></div></div></div><br>
++</pre></div></div><br></div></div></div></div><br><div class="Content2 GTitle1"><div class="MainBlock2"><div class="Content"><h1 class="Title2 Center" id="Branches"><a class="Link3" href="#">Branches</a></h1><div class="Body3"><br>Une <b>branche </b>est une ligne de développement qui est créée à chaque fois qu'on diverge de la ligne principale de développement. C'est le cas rencontré lorsqu'on réalise un correctif ou une évolution.<br><br><div class="Content0 GSummary2"><div class="Row26">Summary 2</div></div><br><h2 class="Title7 GTitle2" id="Branches-Creer-un-projet"><a class="Link9" href="#Branches">Créer un projet</a></h2><br>On souhaite crée un projet C++ qui affiche le message (Bonjour tout le monde) à partir d'un fichier unique (main.cpp).<br><br><h3 class="Title8 GTitle3">Créer le répertoire de projet</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">mkdir repo</pre></div></div><br><h3 class="Title8 GTitle3">Initialiser le dépôt Git</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">cd repo
+git init</pre></div></div><br><h3 class="Title8 GTitle3">Créer le programme principal (main.cpp)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="c_cpp">//===============================================
+#include &lt;iostream&gt;
+//===============================================
+int main(int argc, char** argv) {
+    std::cout &lt;&lt; "Bonjour tout le monde\n";
+    return 0;
+}
+//===============================================</pre></div></div><br><h3 class="Title8 GTitle3">Indexer tous les fichiers</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git add --all</pre></div></div><br><h3 class="Title8 GTitle3">Valider les modifications</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git commit -m "Developpement standard avec un fichier unique"
+
+[master (root-commit) f898194] Developpement standard avec un fichier unique
+ 1 file changed, 8 insertions(+)
+ create mode 100644 main.cpp</pre></div></div><br><h3 class="Title8 GTitle3">Affichier l'historique des commits</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git log
+
+commit f898194fd22ceab0de0b4bb91adb76cb8ad162ad (HEAD -&gt; master)
+Author: Gerard KESSE &lt;tiakagerard@hotmail.com&gt;
+Date:   Sat Jun 26 22:39:16 2021 +0200
+
+    Developpement standard avec un fichier unique</pre></div></div><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+* master</pre></div></div><br><h2 class="Title7 GTitle2" id="Branches-Creer-une-branche"><a class="Link9" href="#Branches">Créer une branche</a></h2><br>On souhaite crée un module (GHello) qui se charge de la définition de la fonction d'affichage du message. La philosophie de cette architecture étant basée sur le développement modulaire qui est totalement opposé à la philosophie du fichier unique ; Il est donc préférable de créer une nouvelle branche pour gérer cette nouvelle architecture.<br><br><h3 class="Title8 GTitle3">Basculer sur la branche (master)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout master
+<br>Already on 'master'</pre></div></div><br><h3 class="Title8 GTitle3">Vérifier que le répertoire de projet est propre</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git status
+
+On branch master
+nothing to commit, working tree clean</pre></div></div><br><h3 class="Title8 GTitle3">Créer la branche (modular)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch modular</pre></div></div><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+* master
+  modular</pre></div></div><br><h3 class="Title8 GTitle3">Basculer sur la branche (modular)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout modular
+
+Switched to branch 'modular'</pre></div></div><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+  master
+* modular</pre></div></div><br><h3 class="Title8 GTitle3">Créer le programme principal (main.cpp)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="c_cpp">//===============================================
+#include "GHello.h"
+//===============================================
+int main(int argc, char** argv) {
+    sayHello();
+    return 0;
+}
+//===============================================</pre></div></div><br><h3 class="Title8 GTitle3">Créer le fichier header du module (GHello.h)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="c_cpp">//===============================================
+#ifndef _GHello_
+#define _GHello_
+//===============================================
+#include &lt;iostream&gt;
+//===============================================
+void sayHello();
+//===============================================
+#endif
+//===============================================</pre></div></div><br><h3 class="Title8 GTitle3">Créer le fichier source du module (GHello.cpp)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="c_cpp">//===============================================
+#include "GHello.h"
+//===============================================
+void sayHello() {
+    std::cout &lt;&lt; "Bonjour tout le monde\n";
+}
+//===============================================</pre></div></div><br><h3 class="Title8 GTitle3">Affichier l'état du dépôt</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git status
+
+On branch modular
+Changes not staged for commit:
+  (use "git add &lt;file&gt;..." to update what will be committed)
+  (use "git restore &lt;file&gt;..." to discard changes in working directory)
+        modified:   main.cpp
+
+Untracked files:
+  (use "git add &lt;file&gt;..." to include in what will be committed)
+        GHello.cpp
+        GHello.h
+
+no changes added to commit (use "git add" and/or "git commit -a")</pre></div></div><br><h3 class="Title8 GTitle3">Indexer tous les fichiers</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git add --all</pre></div></div><br><h3 class="Title8 GTitle3">Affichier l'état du dépôt</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git status
+
+On branch modular
+Changes to be committed:
+  (use "git restore --staged &lt;file&gt;..." to unstage)
+        new file:   GHello.cpp
+        new file:   GHello.h
+        modified:   main.cpp</pre></div></div><br><h3 class="Title8 GTitle3">Valider les modifications</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git commit -m "Developpement modulaire avec plusieurs fichiers"
+<br>[modular 26f51a9] Developpement modulaire avec plusieurs fichiers
+ 3 files changed, 19 insertions(+), 2 deletions(-)
+ create mode 100644 GHello.cpp
+ create mode 100644 GHello.h</pre></div></div><br><h3 class="Title8 GTitle3">Affichier l'historique des commits</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git log
+
+commit 26f51a92e2838c5d0bf7b69a15b1b4941fe753bd (HEAD -&gt; modular)
+Author: Gerard KESSE &lt;tiakagerard@hotmail.com&gt;
+Date:   Sat Jun 26 23:44:43 2021 +0200
+
+    Developpement modulaire avec plusieurs fichiers
+
+commit f898194fd22ceab0de0b4bb91adb76cb8ad162ad (master)
+Author: Gerard KESSE &lt;tiakagerard@hotmail.com&gt;
+Date:   Sat Jun 26 22:39:16 2021 +0200
+
+    Developpement standard avec un fichier unique</pre></div></div><br><h2 class="Title7 GTitle2" id="Branches-Comprendre-l-importance-des-branches"><a class="Link9" href="#Branches">Comprendre l'importance des branches</a></h2><br>Git conserve l'historique des modifications dans toutes les <b>branches</b>.<br><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+* master
+  modular</pre></div></div><br><h3 class="Title8 GTitle3">Basculer sur la branche (master)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout master</pre></div></div><br><div class="Img3 GImage"><img src="/Tutoriels/Version_Control/Git/img/i_branch_master.png" alt="/Tutoriels/Version_Control/Git/img/i_branch_master.png"></div><br><h3 class="Title8 GTitle3">Basculer sur la branche (modular)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout modular</pre></div></div><br><div class="Img3 GImage"><img src="/Tutoriels/Version_Control/Git/img/i_branch_modular.png" alt="/Tutoriels/Version_Control/Git/img/i_branch_modular.png"></div><br><h2 class="Title7 GTitle2" id="Branches-Merger-des-branches"><a class="Link9" href="#Branches">Merger des branches</a></h2><br>Après l'analyse des 2 architectures, on valide la philosophie de l'architecture du développement modulaire. Cela signifie qu'on valide de <b>merger </b>la branche (modular) dans la branche (master).<br><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+* master
+  modular</pre></div></div><br><h3 class="Title8 GTitle3">Basculer sur la branche (master)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout master</pre></div></div><br><h3 class="Title8 GTitle3">Vérifier que le répertoire de projet est propre</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git status
+
+On branch master
+nothing to commit, working tree clean</pre></div></div><br><h3 class="Title8 GTitle3">Basculer sur la branche (modular)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout modular</pre></div></div><br><h3 class="Title8 GTitle3">Vérifier que le répertoire de projet est propre</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git status
+
+On branch modular
+nothing to commit, working tree clean</pre></div></div><br><h3 class="Title8 GTitle3">Merger les branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout master
+git merge modular
+
+Updating f898194..26f51a9
+Fast-forward
+ GHello.cpp |  7 +++++++
+ GHello.h   | 10 ++++++++++
+ main.cpp   |  4 ++--
+ 3 files changed, 19 insertions(+), 2 deletions(-)
+ create mode 100644 GHello.cpp
+ create mode 100644 GHello.h</pre></div></div><br><h2 class="Title7 GTitle2" id="Branches-Supprimer-une-branche"><a class="Link9" href="#Branches">Supprimer une branche</a></h2><br>Maintenant que la branche (modular) a été mergée dans la branche (master) et qu'on n'en a plus besoin ; On peut donc la <b>supprimer</b>.<br><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+* master
+  modular</pre></div></div><br><h3 class="Title8 GTitle3">Basculer sur la branche (master)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git checkout master</pre></div></div><br><h3 class="Title8 GTitle3">Supprimer la branche (modular)</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch -d modular
+
+Deleted branch modular (was 26f51a9).</pre></div></div><br><h3 class="Title8 GTitle3">Afficher la liste des branches</h3><br><div class="GCode1"><div class="Code2"><pre class="AceCode" data-state="off" data-mode="sh">git branch
+
+* master</pre></div></div><br></div></div></div></div><br>
