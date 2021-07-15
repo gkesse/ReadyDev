143437e7801e4410e49bb8c6ebb81cc8ced981ab
Gerard KESSE <tiakagerard@hotmail.com>
2021-07-15 23:31:05 +0200

    Initial Commit

--- Tutoriels/Resources/ProjectBox/index.php
+++ Tutoriels/Resources/ProjectBox/index.php
@@ -1,11 +1,11 @@
 <?php
     require $_SERVER["DOCUMENT_ROOT"]."/php/class/GAutoloadRegister.php";
     
-    $m_data = GJson::Instance()->getData("data/json/Project_Euler.json");
+    $m_data = GJson::Instance()->getData("data/json/ProjectBox.json");
     
-    GConfig::Instance()->setData("title", "Project Euler");
+    GConfig::Instance()->setData("title", "ProjectBox");
     GConfig::Instance()->setData("menu", "Tutoriels");
-    GConfig::Instance()->setData("view", "Project_Euler");
+    GConfig::Instance()->setData("view", "ProjectBox");
     GConfig::Instance()->setData("link", $m_data["links"]);
 
     require $_SERVER["DOCUMENT_ROOT"]."/php/header.php";
