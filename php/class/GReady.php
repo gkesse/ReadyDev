a5e8f025479b7671e1ded3afa7a63e0fd568dbd4
Gerard KESSE <tiakagerard@hotmail.com>
2022-05-18 13:21:12 +0200

    initial commit

--- php/class/GReady.php
+++ php/class/GReady.php
@@ -1,8 +1,12 @@
 <?php   
     class GReady extends GObject {
         //===============================================
+        private $foundOn;
+        //===============================================
         public function __construct() {
-            parent::__construct();            
+            parent::__construct();
+            $this->createDoms();
+            $this->foundOn = false;
         }
         //===============================================
         public function run() {
@@ -22,12 +26,28 @@
         }
         //===============================================
         public function onBody() {
+            echo sprintf("<div class='MainBody'>\n");
+            $this->onAdmin();
+            $this->onError();
+            echo sprintf("</div>\n");
+        }
+        //===============================================
+        public function onAdmin() {
             $lPageObj = new GPage();
             $lPage = $lPageObj->getPageId();
-            $lContent = $lPage;
-            echo sprintf("<div class='MainBody'>");
-            echo $lContent;
-            echo sprintf("</div>");            
+            if($lPage != "/home/admin") return;
+            $this->foundOn = true;
+            echo sprintf("<div>\n");
+            echo sprintf("<div>ADMIN</div>\n");
+            echo sprintf("</div>\n");
+        }
+        //===============================================
+        public function onError() {
+            if($this->foundOn) return;
+            $lPageObj = new GPage();
+            $lPage = $lPageObj->getPageId();
+            GLog::Instance()->addError(sprintf(
+                "<i class='fa fa-exclamation-triangle'></i> PAGE NON TROUVEE", $lPage));
         }
         //===============================================
     }
