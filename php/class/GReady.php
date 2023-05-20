b643ba1ab3ff63a54ef68cf8a052e8a578ce9e90
Gerard KESSE <tiakagerard@hotmail.com>
2023-05-20 10:45:47 +0200

    page : gestion de la page xdebug

--- php/class/GReady.php
+++ php/class/GReady.php
@@ -91,6 +91,7 @@ class GReady extends GObject {
         // tutoriels/php
         $lObj2 = $lMenu->addMenu("tutoriels", "PHP", "PHP", "", $lObj);
         $lMenu->addMenu("tutoriels", "Cours", "PHP", "/home/tutoriels/php/cours", $lObj2);
+        $lMenu->addMenu("tutoriels", "Xdebug", "Xdebug", "/home/tutoriels/php/xdebug", $lObj2);
         $lMenu->addMenu("tutoriels", "Smarty", "Smarty", "/home/tutoriels/php/smarty", $lObj2);
         // tutoriels/css
         $lObj2 = $lMenu->addMenu("tutoriels", "CSS", "CSS", "", $lObj);
