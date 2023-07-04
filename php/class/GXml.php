640ce50dff4e08568e0c2954591b8ff17bf16fef
Gerard KESSE <tiakagerard@hotmail.com>
2023-07-04 23:24:32 +0200

    page php

--- php/class/GXml.php
+++ php/class/GXml.php
@@ -11,6 +11,64 @@ class GXml {
         
     }
     //===============================================
+    public function createDoc() {
+        $this->m_doc = new \DOMDocument("1.0", "UTF-8");
+        if(!$this->m_doc) return false;
+        $this->m_doc->preserveWhiteSpace = false;
+        $this->m_doc->formatOutput = true;
+        $this->m_node = $this->m_doc->createElement("rdv");
+        if(!$this->m_node) return false;
+        $this->m_doc->appendChild($this->m_node);
+        return true;
+    }
+    //===============================================
+    public function loadXml($_data) {
+        $_data = trim($_data);
+        if($_data == "") return false;
+        $this->m_doc = new \DOMDocument("1.0", "UTF-8");
+        if(!$this->m_doc) return false;
+        if(!str_starts_with($_data, "<?xml")) {
+            $_data = sprintf("<?xml version='1.0' encoding='UTF-8'?>\n%s", $_data);
+        }
+        $this->m_doc->preserveWhiteSpace = false;
+        $this->m_doc->formatOutput = true;
+        if(!$this->m_doc->loadXml($_data)) return false;
+        $this->m_node = $this->m_doc->documentElement;
+        return true;
+    }
+    //===============================================
+    public function loadNode($_root, $_data) {
+        $_data = trim($_data);
+        if($_data == "") return false;
+        if(!$_root->m_doc) return false;
+        if(!$this->m_node) return false;
+        $lDom = new \DOMDocument("1.0", "UTF-8");
+        $lDom->preserveWhiteSpace = false;
+        $lDom->formatOutput = true;
+        if(!$lDom->loadXML($_data)) return false;
+        $lChild = $lDom->documentElement->firstElementChild;
+        while($lChild) {
+            $lNode = $_root->m_doc->importNode($lChild, true);
+            $this->m_node->appendChild($lNode);
+            $lChild = $lChild->nextElementSibling;
+        }
+        return true;
+    }
+    //===============================================
+    public function addObj($_root, $_name) {
+        $lNode = $_root->m_doc->createElement($_name);
+        $this->m_node->appendChild($lNode);
+        return $lNode;
+    }
+    //===============================================
+    public function addValue($_root, $_name, $_value, $_isCData = false) {
+        $lDom = new GXml();
+        $lDom->m_node = $_root->m_doc->createElement($_name);
+        $lDom->setValue($_root, $_value, $_isCData);
+        $this->m_node->appendChild($lDom->m_node);
+        return $this->m_node;
+    }
+    //===============================================
     public function setNode($_node) {
         $this->m_node = $_node;
     }
@@ -34,49 +92,6 @@ class GXml {
             $lData = $this->m_node->textContent;
         }
         return $lData;
-    }
-    //===============================================
-    public function createDoc() {
-        $this->m_doc = new \DOMDocument("1.0", "UTF-8");
-        $this->m_doc->preserveWhiteSpace = false;
-        $this->m_doc->formatOutput = true;
-        $this->m_node = $this->m_doc->createElement("rdv");
-        $this->m_doc->appendChild($this->m_node);
-        return true;
-    }
-    //===============================================
-    public function loadXml($_data) {
-        $_data = trim($_data);
-        if($_data == "") return false;
-        $this->m_doc = new \DOMDocument("1.0", "UTF-8");
-        if(!$this->m_doc) return false;
-        $lXml = "<?xml";
-        if(substr($_data, 0, strlen($lXml)) != $lXml) {
-            $_data = sprintf("<?xml version='1.0' encoding='UTF-8'?>\n%s", $_data);
-        }
-        $this->m_doc->preserveWhiteSpace = false;
-        $this->m_doc->formatOutput = true;
-        $this->m_doc->loadXml($_data);
-        $this->m_node = $this->m_doc->documentElement;
-        return true;
-    }
-    //===============================================
-    public function loadNode($_root, $_data) {
-        $_data = trim($_data);
-        if($_data == "") return false;
-        if(!$_root->m_doc) return false;
-        if(!$this->m_node) return false;
-        $lDom = new \DOMDocument("1.0", "UTF-8");
-        $lDom->preserveWhiteSpace = false;
-        $lDom->formatOutput = true;
-        $lDom->loadXML($_data);
-        $lChild = $lDom->documentElement->firstChild;
-        while($lChild) {
-            $lNode = $_root->m_doc->importNode($lChild, true);
-            $this->m_node->appendChild($lNode);
-            $lChild = $lChild->nextSibling;
-        }
-        return true;
     }
     //===============================================
     public function existNode($_root, $_path) {
@@ -106,20 +121,6 @@ class GXml {
         if($_path[0] == "/") $lNode = $_root->m_node;
         $lNodes = $lXPath->query($_path, $lNode);
         return $lNodes->length;
-    }
-    //===============================================
-    public function addObj($_root, $_name) {
-        $lNode = $_root->m_doc->createElement($_name);
-        $this->m_node->appendChild($lNode);
-        return $lNode;
-    }
-    //===============================================
-    public function addValue($_root, $_name, $_value, $_isCData = false) {
-        $lDom = new GXml();
-        $lDom->m_node = $_root->m_doc->createElement($_name);
-        $lDom->setValue($_root, $_value, $_isCData);
-        $this->m_node->appendChild($lDom->m_node);
-        return $this->m_node;
     }
     //===============================================
     public function toString() {
