dbf23b722de680ca0df4e90391cca00e072f142f
Gerard KESSE <tiakagerard@hotmail.com>
2025-05-27 16:58:14 +0200

    Initialisation de la zone de cartographie de la fenêtre principale

--- data/cache/page/readydev/home/tutoriels/main.php
+++ data/cache/page/readydev/home/tutoriels/main.php
@@ -6,10 +6,6 @@
 </div>
 <div class="Parallax5" style="background-color: rgb(128, 51, 0);"><br><b>ReadyDev</b> est une Plateforme de Développement Continu.<br>C'est un site web développé en <b>PHP</b> et maintenu par <b>Gérard KESSE</b>.<br>Vous trouverez sur ce site des <b>cours</b> et des <b>tutoriels</b> adaptés aux <b>sciences de l'ingénieur</b>.<br><br><div class="GSummary1"><div class="GSummary11 Summary1">
 <i class="Summary2 fa fa-book"></i>
-<a class="Summary3" href="#app-cpp">Application C++</a>
-</div>
-<div class="GSummary11 Summary1">
-<i class="Summary2 fa fa-book"></i>
 <a class="Summary3" href="#cpp">Développement C++</a>
 </div>
 <div class="GSummary11 Summary1">
@@ -36,25 +32,14 @@
 <div class="Section2">
 <div class="Section3">
 <h1 class="Section4">
-<a class="Section5" href="#" id="app-cpp">Application C++</a>
+<a class="Section5" href="#" id="cpp">Développement C++</a>
 </h1>
 <div class="Section6"><br><div class="GTuto1 Tuto1">
 <a class="GTuto2 Tuto2" href="/home/tutoriels/cpp/application/systeme-navigation-gps">
 <div class="Tuto3"><i class="Tuto6 fa fa-connectdevelop"></i></div>
-<div class="Tuto4"><div class="Tuto7">Système de navigation par<br>GPS</div></div>
+<div class="Tuto4"><div class="Tuto7">Système de navigation par <br>GPS</div></div>
 <div class="Tuto5"><div class="Tuto8">Programmez en<br>C++ / Qt</div></div>
-</a>
-</div><br></div>
-</div>
-</div>
-</div><br><div class="GSection1 Section1">
-<div class="Section2">
-<div class="Section3">
-<h1 class="Section4">
-<a class="Section5" href="#" id="cpp">Développement C++</a>
-</h1>
-<div class="Section6"><br><div class="GTuto1 Tuto1">
-<a class="GTuto2 Tuto2" href="/home/tutoriels/cpp/cours/apprendre-cpp">
+</a><a class="GTuto2 Tuto2" href="/home/tutoriels/cpp/cours/apprendre-cpp">
 <div class="Tuto3"><i class="Tuto6 fa fa-connectdevelop"></i></div>
 <div class="Tuto4"><div class="Tuto7">Apprendre<br>C++</div></div>
 <div class="Tuto5"><div class="Tuto8">Programmez en<br>C++</div></div>
